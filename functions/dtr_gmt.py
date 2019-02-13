dfcf6c698381dbc8427812137019bf38f850ebac
bschmidt <bschmidt@pik-potsdam.com>
2019-02-13 15:54:59 +0100

    first functions

---
+++ functions/dtr_gmt.py
@@ -0,0 +1,9 @@
+#!/home/bschmidt/.programs/anaconda3/envs/detrending/bin/python3
+
+def dtr_gmt(data, hws=182):
+    """This function computes global spatial of xarray dataset.\n
+    Input:\n
+    data:\tXarray dataset
+    Output:\t Xarray DataArray of spatial global average values"""
+
+    
