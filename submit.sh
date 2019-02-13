b2c068317d5dca523e36d4d1d694362ccdd2c425
bschmidt <bschmidt@pik-potsdam.com>
2019-02-13 15:58:10 +0100

    first functions

--- submit.sh
+++ submit.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-#SBATCH --qos=short
-#SBATCH --job-name=dtr_wind_smooth
-#SBATCH --account=
-#SBATCH --output=output/dtr_wind_smooth.out
-#SBATCH --error=output/dtr_wind_smooth.err
+#SBATCH --qos=priority
+##SBATCH --partition=priority
+#SBATCH --job-name=dtr_gmt
+#SBATCH --account=isipedia
+#SBATCH --output=output/dtr_gmt.out
+#SBATCH --error=output/dtr_gmt.err
 #SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user={{user}}@pik-potsdam.de
+#SBATCH --mail-user=bschmidt@pik-potsdam.de
 
 # # block one node completely to get all its memory.
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --exclusive
 
-source /home/bschmidt/.programs/anaconda3/bin/activate py2a
-srun python3 wind_smooth.py
+source /home/bschmidt/.programs/anaconda3/bin/activate detrending
+srun python3 gmt.py
