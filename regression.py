648c9cd222f91ce04bd4500bc63be0731a027e7b
Matthias Mengel <mengel@pik-potsdam.de>
2019-03-14 18:10:33 +0100

    writing slope and intercept to netcdf via iris.
    catching results from joblib via a python list and
    putting it into an iris cube is very fast.
    iris cube reads and writes are not used within joblib anymore.
    this may be one reason for the speed.

--- regression.py
+++ regression.py
@@ -3,19 +3,19 @@ from scipy import stats
 
 
 def linear_regr_per_gridcell(np_data_to_detrend,gmt_on_each_day,doy,lati,loni):
-    
+
     """ minimal version of a linear regression per grid cell """
-    
+
     data_of_doy = np_data_to_detrend[doy::365,lati,loni]
     gmt_of_doy = gmt_on_each_day[doy::365]
     return  stats.linregress(gmt_of_doy,data_of_doy)
 
 
 def run_serial_linear_regr():
-    
+
     """ calculate linear regression stats for all days of years and all grid cells.
     classic serial looping. Return a list of all regression stats. """
-    
+
     results = []
     for doy in np.arange(days_of_year):
         for lati in np.arange(np_data_to_detrend.shape[1]):
@@ -23,7 +23,12 @@ def run_serial_linear_regr():
                 result = linear_regr_per_gridcell(
                             np_data_to_detrend,gmt_on_each_day,doy,lati,loni)
                 results.append(result)
-                
+
     return results
 
 
+if __name__ == "__main__":
+
+    """ add a quick test for one grid cell of our regression algorithm here. """
+
+    pass
--- run_regression.py
+++ run_regression.py
@@ -30,7 +30,7 @@ doys = np.arange(days_of_year)
 
 
 def run_parallel_linear_regr(**kwargs):
-    
+
     """ calculate linear regression stats for all days of years and all grid cells.
     joblib implementation. Return a list of all regression stats. """
 
@@ -41,9 +41,63 @@ def run_parallel_linear_regr(**kwargs):
     return results
 
 
+### the following functions maybe moved to an "io and iris file" later. ###
+
+def create_doy_cube(array,original_cube_coords, **kwargs):
+
+    """ create an iris cube from a plain numpy array.
+    First dimension is always days of the year. Second and third are lat and lon
+    and are taken from the input data. """
+
+    doy_coord = iris.coords.DimCoord(np.arange(1.,366.), var_name="day_of_year")
+
+    cube = iris.cube.Cube(array,
+            dim_coords_and_dims=[(doy_coord, 0),
+             (original_cube_coords('latitude'), 1),
+             (original_cube_coords('longitude'), 2),
+            ], **kwargs)
+
+    return cube
+
+
+def write_linear_regression_stats(shape_of_input, original_cube_coords,
+        results, file_to_write):
+
+    """ write linear regression statistics to a netcdf file. This function is specific
+    to the output of the scipy.stats.linregress output.
+    TODO: make this more flexible to include more stats. """
+
+    intercepts = np.zeros([days_of_year, shape_of_input[1],
+                       shape_of_input[2]])
+    slopes = np.zeros_like(intercepts)
+
+    i = 0
+    for doy in np.arange(days_of_year):
+        for lati in latis:
+            for loni in lonis:
+                intercepts[doy,lati,loni] = results[i].intercept
+                slopes[doy,lati,loni] = results[i].slope
+                i = i + 1
+
+    scube = create_doy_cube(slopes, original_cube_coords, var_name="slope_linear_regr")
+    icube = create_doy_cube(intercepts, original_cube_coords, var_name="intercept_linear_regr")
+
+    iris.fileformats.netcdf.save([scube,icube], file_to_write)
+
+
 if __name__ == "__main__":
 
-    FTIME = datetime.now()
-    run_parallel_linear_regr(n_jobs=3)
-    duration = datetime.now() - FTIME
-    print('This took', duration.total_seconds(), 'seconds.')
+    TIME0 = datetime.now()
+    results = run_parallel_linear_regr(n_jobs=3)
+    TIME1 = datetime.now()
+    duration = TIME1 - TIME0
+    print('Calculation took', duration.total_seconds(), 'seconds.')
+
+    file_to_write = os.path.join(s.data_dir, "testfile.nc4")
+    # due to a bug in iris I guess, I cannot overwrite existing files. Remove before.
+    if os.path.exists(file_to_write): os.remove(file_to_write)
+    write_linear_regression_stats(np_data_to_detrend.shape, data_to_detrend.coord,
+        results, file_to_write)
+    TIME2 = datetime.now()
+    duration = TIME2 - TIME1
+    print('Saving took', duration.total_seconds(), 'seconds.')
