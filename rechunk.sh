b69eade544bc0b0a42d5a683f113b78125f01691
bschmidt <bschmidt@pik-potsdam.com>
2019-03-19 14:57:36 +0100

    leap year removal

--- rechunk.sh
+++ rechunk.sh
@@ -15,12 +15,49 @@
 #SBATCH --exclusive
 #
 
-nccopy -u -k 'nc4' -d 0 -m 32M -c time/,lat/1,lon/ /home/bschmidt/temp/gswp3/pr_rm_gswp3_1901_2010.nc4 /home/bschmidt/temp/gswp3/pr_rm_rechunked_gswp3_1901_2010.nc4
-nccopy -u -k 'nc4' -d 0 -m 32M -c time/,lat/1,lon/ /home/bschmidt/temp/gswp3/ps_rm_gswp3_1901_2010.nc4 /home/bschmidt/temp/gswp3/ps_rm_rechunked_gswp3_1901_2010.nc4
-nccopy -u -k 'nc4' -d 0 -m 32M -c time/,lat/1,lon/ /home/bschmidt/temp/gswp3/huss_rm_gswp3_1901_2010.nc4 /home/bschmidt/temp/gswp3/huss_rm_rechunked_gswp3_1901_2010.nc4
-nccopy -u -k 'nc4' -d 0 -m 32M -c time/,lat/1,lon/ /home/bschmidt/temp/gswp3/rhs_rm_gswp3_1901_2010.nc4 /home/bschmidt/temp/gswp3/rhs_rm_rechunked_gswp3_1901_2010.nc4
-nccopy -u -k 'nc4' -d 0 -m 32M -c time/,lat/1,lon/ /home/bschmidt/temp/gswp3/rlds_rm_gswp3_1901_2010.nc4 /home/bschmidt/temp/gswp3/rlds_rm_rechunked_gswp3_1901_2010.nc4
-nccopy -u -k 'nc4' -d 0 -m 32M -c time/,lat/1,lon/ /home/bschmidt/temp/gswp3/rsds_rm_gswp3_1901_2010.nc4 /home/bschmidt/temp/gswp3/rsds_rm_rechunked_gswp3_1901_2010.nc4
-nccopy -u -k 'nc4' -d 0 -m 32M -c time/,lat/1,lon/ /home/bschmidt/temp/gswp3/tasmax_rm_gswp3_1901_2010.nc4 /home/bschmidt/temp/gswp3/tasmax_rm_rechunked_gswp3_1901_2010.nc4
-nccopy -u -k 'nc4' -d 0 -m 32M -c time/,lat/1,lon/ /home/bschmidt/temp/gswp3/tasmin_rm_gswp3_1901_2010.nc4 /home/bschmidt/temp/gswp3/tasmin_rm_rechunked_gswp3_1901_2010.nc4
-nccopy -u -k 'nc4' -d 0 -m 32M -c time/,lat/1,lon/ /home/bschmidt/temp/gswp3/tas_rm_gswp3_1901_2010.nc4 /home/bschmidt/temp/gswp3/tas_rm_rechunked_gswp3_1901_2010.nc4
+cdo delete,month=2,day=29 /home/bschmidt/temp/gswp3/pr_rm_gswp3_1901_2010.nc4 temp.nc
+mv temp.nc /home/bschmidt/temp/gswp3/pr_rm_gswp3_1901_2010.nc4
+echo 'Removed leap day from pr'
+cdo delete,month=2,day=29 /home/bschmidt/temp/gswp3/ps_rm_gswp3_1901_2010.nc4 temp.nc
+mv temp.nc /home/bschmidt/temp/gswp3/ps_rm_gswp3_1901_2010.nc4
+echo 'Removed leap day from ps'
+cdo delete,month=2,day=29 /home/bschmidt/temp/gswp3/huss_rm_gswp3_1901_2010.nc4 temp.nc
+mv temp.nc /home/bschmidt/temp/gswp3/huss_rm_gswp3_1901_2010.nc4
+echo 'Removed leap day from huss'
+cdo delete,month=2,day=29 /home/bschmidt/temp/gswp3/rhs_rm_gswp3_1901_2010.nc4 temp.nc
+mv temp.nc /home/bschmidt/temp/gswp3/rhs_rm_gswp3_1901_2010.nc4
+echo 'Removed leap day from rhs'
+cdo delete,month=2,day=29 /home/bschmidt/temp/gswp3/rlds_rm_gswp3_1901_2010.nc4 temp.nc
+mv temp.nc /home/bschmidt/temp/gswp3/rlds_rm_gswp3_1901_2010.nc4
+echo 'Removed leap day from rlds'
+cdo delete,month=2,day=29 /home/bschmidt/temp/gswp3/rsds_rm_gswp3_1901_2010.nc4 temp.nc
+mv temp.nc /home/bschmidt/temp/gswp3/rsds_rm_gswp3_1901_2010.nc4
+echo 'Removed leap day from rsds'
+cdo delete,month=2,day=29 /home/bschmidt/temp/gswp3/tasmax_rm_gswp3_1901_2010.nc4 temp.nc
+mv temp.nc /home/bschmidt/temp/gswp3/tasmax_rm_gswp3_1901_2010.nc4
+echo 'Removed leap day from tasmax'
+cdo delete,month=2,day=29 /home/bschmidt/temp/gswp3/tasmin_rm_gswp3_1901_2010.nc4 temp.nc
+mv temp.nc /home/bschmidt/temp/gswp3/tasmin_rm_gswp3_1901_2010.nc4
+echo 'Removed leap day from tasmin'
+cdo delete,month=2,day=29 /home/bschmidt/temp/gswp3/tas_rm_gswp3_1901_2010.nc4 temp.nc
+mv temp.nc /home/bschmidt/temp/gswp3/tas_rm_gswp3_1901_2010.nc4
+echo 'Removed leap day from tas'
+
+nccopy -u -k 'nc4' -m 32G -c time/4018,lat/1,lon/720 /home/bschmidt/temp/gswp3/pr_rm_gswp3_1901_2010.nc4 /home/bschmidt/temp/gswp3/pr_rm_rechunked_gswp3_1901_2010.nc4
+echo 'rechunked pr'
+nccopy -u -k 'nc4' -m 32G -c time/4018,lat/1,lon/720 /home/bschmidt/temp/gswp3/ps_rm_gswp3_1901_2010.nc4 /home/bschmidt/temp/gswp3/ps_rm_rechunked_gswp3_1901_2010.nc4
+echo 'rechunked ps'
+nccopy -u -k 'nc4' -m 32G -c time/4018,lat/1,lon/720 /home/bschmidt/temp/gswp3/huss_rm_gswp3_1901_2010.nc4 /home/bschmidt/temp/gswp3/huss_rm_rechunked_gswp3_1901_2010.nc4
+echo 'rechunked huss'
+nccopy -u -k 'nc4' -m 32G -c time/4018,lat/1,lon/720 /home/bschmidt/temp/gswp3/rhs_rm_gswp3_1901_2010.nc4 /home/bschmidt/temp/gswp3/rhs_rm_rechunked_gswp3_1901_2010.nc4
+echo 'rechunked rhs'
+nccopy -u -k 'nc4' -m 32G -c time/4018,lat/1,lon/720 /home/bschmidt/temp/gswp3/rlds_rm_gswp3_1901_2010.nc4 /home/bschmidt/temp/gswp3/rlds_rm_rechunked_gswp3_1901_2010.nc4
+echo 'rechunked rlds'
+nccopy -u -k 'nc4' -m 32G -c time/4018,lat/1,lon/720 /home/bschmidt/temp/gswp3/rsds_rm_gswp3_1901_2010.nc4 /home/bschmidt/temp/gswp3/rsds_rm_rechunked_gswp3_1901_2010.nc4
+echo 'rechunked rsds'
+nccopy -u -k 'nc4' -m 32G -c time/4018,lat/1,lon/720 /home/bschmidt/temp/gswp3/tasmax_rm_gswp3_1901_2010.nc4 /home/bschmidt/temp/gswp3/tasmax_rm_rechunked_gswp3_1901_2010.nc4
+echo 'rechunked tasmax'
+nccopy -u -k 'nc4' -m 32G -c time/4018,lat/1,lon/720 /home/bschmidt/temp/gswp3/tasmin_rm_gswp3_1901_2010.nc4 /home/bschmidt/temp/gswp3/tasmin_rm_rechunked_gswp3_1901_2010.nc4
+echo 'rechunked tasmin'
+nccopy -u -k 'nc4' -m 32G -c time/4018,lat/1,lon/720 /home/bschmidt/temp/gswp3/tas_rm_gswp3_1901_2010.nc4 /home/bschmidt/temp/gswp3/tas_rm_rechunked_gswp3_1901_2010.nc4
+echo 'rechunked tas'
