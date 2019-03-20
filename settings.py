ead54349e70628d6869e25e8cec380664b57dea5
bschmidt <bschmidt@pik-potsdam.com>
2019-03-20 17:15:10 +0100

    added source file for test_data creation

--- settings.py
+++ settings.py
@@ -10,7 +10,9 @@ else:
 	data_dir = "/home/bschmidt/temp/gswp3/"
 
 #  handle job specifications
-n_jobs = 8  # number of childprocesses created by the job
+n_jobs = 16  # number of childprocesses created by the job
 
 gmt_file = 'test_ssa_gmt.nc4'
-to_detrend_file = 'pr_rm_rechunked_gswp3_1901_2010.nc4'
+to_detrend_file = 'test_data_rlds.nc4'
+base_file = 'rlds_rm_rechunked_gswp3_1901_2010.nc4'
+regression_outfile = 'rlds_regression.nc4'
