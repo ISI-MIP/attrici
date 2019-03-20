be45aab4395e5ee5ce7ef54ee70ab4db51730323
bschmidt <bschmidt@pik-potsdam.com>
2019-03-20 17:28:40 +0100

    set variable argument

--- settings.py
+++ settings.py
@@ -12,7 +12,10 @@ else:
 #  handle job specifications
 n_jobs = 16  # number of childprocesses created by the job
 
+variable = 'tas'
+
+
 gmt_file = 'test_ssa_gmt.nc4'
-to_detrend_file = 'test_data_rlds.nc4'
-base_file = 'rlds_rm_rechunked_gswp3_1901_2010.nc4'
-regression_outfile = 'rlds_regression.nc4'
+to_detrend_file = 'test_data_' + variable + '.nc4'
+base_file = variable + '_rm_rechunked_gswp3_1901_2010.nc4'
+regression_outfile = variable + '_regression.nc4'
