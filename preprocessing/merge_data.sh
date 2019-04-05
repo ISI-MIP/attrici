21f35b1fa276101f57329e7dc3cd956110d0f84c
bschmidt <bschmidt@pik-potsdam.com>
2019-04-05 18:57:19 +0200

    Merge input data to one file from specified startyear to endyear. variable is read from settings.py. Will ask for input so dont run unsupervised

--- preprocessing/merge_data.sh
+++ preprocessing/merge_data.sh
@@ -11,5 +11,24 @@ dataset=gswp3
 startyear=1901
 endyear=2010
 datapath=/p/tmp/bschmidt/${dataset}/
-cdo mergetime ${datapath}${variable}_${dataset}_*.nc* \
-    ${datapath}${variable}_${dataset}_${startyear}_${endyear}.nc4
+outputfile=${datapath}${variable}_${dataset}_${startyear}_${endyear}.nc4
+
+if [ -e ${outputfile} ]; then
+    while true; do
+        read -p "Specified output file exists. Delete?" yn
+        case $yn in
+            [Yy'\r']* ) \
+                echo 'Deleting' $outputfile
+                rm $outputfile 
+                echo 'Merging files!'
+                cdo mergetime ${datapath}${variable}_${dataset}_*.nc* \
+                $outputfile
+            break;;
+            [Nn]* ) \
+                echo 'Aborted'
+                exit;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+fi
+
