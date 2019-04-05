aeb48b8d3b87b0379771c3be563e21e5171ef59d
bschmidt <bschmidt@pik-potsdam.com>
2019-04-05 19:46:00 +0200

    split tasks leap-day-removal and rechunking into two files

--- preprocessing/rechunk.sh
+++ preprocessing/rechunk.sh
@@ -17,13 +17,9 @@
 datafolder=/home/bschmidt/temp/gswp3/
 dataset=gswp3
 variables=( pr ps huss rhs rlds rsds tasmax tasmin tas wind )
-echo 'Remove leap days from and rechunk the following variables'
+echo 'Rechunk the following variables'
 echo $variables
 for i in "${variables[@]}"; do
-    echo "Starting deletion of leap days (29th of feb) for variable" $i
-    cdo delete,month=2,day=29 ${datafolder}${i}_rm_gswp3_1901_2010.nc4 temp.nc
-    mv temp.nc ${datafolder}${i}_gswp3_1901_2010.nc4
-    echo 'Removed leap day from' $i
     echo 'Rechunking' $i
     nccopy -u -k 'nc4' -m 32G -c time/4018,lat/1,lon/720 ${datafolder}${i}_${dataset}_1901_2010_noleap.nc4 \
         ${datafolder}${i}_rechunked_${dataset}_1901_2010_noleap.nc4
