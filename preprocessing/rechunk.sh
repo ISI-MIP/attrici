e9d71eb068f9dae7099df6e137ca99f5c8c38bde
bschmidt <bschmidt@pik-potsdam.com>
2019-04-05 20:58:29 +0200

    changed script to read input from settings.py file

--- preprocessing/rechunk.sh
+++ preprocessing/rechunk.sh
@@ -14,62 +14,34 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --exclusive
 #
-datafolder=/home/bschmidt/temp/gswp3/
-dataset=gswp3
-variables=( pr ps huss rhs rlds rsds tasmax tasmin tas wind )
-echo 'Rechunk the following variables'
-echo $variables
-for i in "${variables[@]}"; do
-    echo 'Rechunking' $i
-    nccopy -u -k 'nc4' -m 32G -c time/4018,lat/1,lon/720 ${datafolder}${i}_${dataset}_1901_2010_noleap.nc4 \
-        ${datafolder}${i}_rechunked_${dataset}_1901_2010_noleap.nc4
-    echo 'rechunked' $i 'for faster access to full timeseries'
-done
-# cdo delete,month=2,day=29 /home/bschmidt/temp/gswp3/ps_rm_gswp3_1901_2010.nc4 temp.nc
-# mv temp.nc /home/bschmidt/temp/gswp3/ps_rm_gswp3_1901_2010.nc4
-# echo 'Removed leap day from ps'
-# cdo delete,month=2,day=29 /home/bschmidt/temp/gswp3/huss_rm_gswp3_1901_2010.nc4 temp.nc
-# mv temp.nc /home/bschmidt/temp/gswp3/huss_rm_gswp3_1901_2010.nc4
-# echo 'Removed leap day from huss'
-# cdo delete,month=2,day=29 /home/bschmidt/temp/gswp3/rhs_rm_gswp3_1901_2010.nc4 temp.nc
-# mv temp.nc /home/bschmidt/temp/gswp3/rhs_rm_gswp3_1901_2010.nc4
-# echo 'Removed leap day from rhs'
-# cdo delete,month=2,day=29 /home/bschmidt/temp/gswp3/rlds_rm_gswp3_1901_2010.nc4 temp.nc
-# mv temp.nc /home/bschmidt/temp/gswp3/rlds_rm_gswp3_1901_2010.nc4
-# echo 'Removed leap day from rlds'
-# cdo delete,month=2,day=29 /home/bschmidt/temp/gswp3/rsds_rm_gswp3_1901_2010.nc4 temp.nc
-# mv temp.nc /home/bschmidt/temp/gswp3/rsds_rm_gswp3_1901_2010.nc4
-# echo 'Removed leap day from rsds'
-# cdo delete,month=2,day=29 /home/bschmidt/temp/gswp3/tasmax_rm_gswp3_1901_2010.nc4 temp.nc
-# mv temp.nc /home/bschmidt/temp/gswp3/tasmax_rm_gswp3_1901_2010.nc4
-# echo 'Removed leap day from tasmax'
-# cdo delete,month=2,day=29 /home/bschmidt/temp/gswp3/tasmin_rm_gswp3_1901_2010.nc4 temp.nc
-# mv temp.nc /home/bschmidt/temp/gswp3/tasmin_rm_gswp3_1901_2010.nc4
-# echo 'Removed leap day from tasmin'
-# cdo delete,month=2,day=29 /home/bschmidt/temp/gswp3/tas_gswp3_1901_2010.nc4 temp.nc
-# mv temp.nc /home/bschmidt/temp/gswp3/tas_gswp3_1901_2010_noleap.nc4
-# echo 'Removed leap day from tas'
-# cdo delete,month=2,day=29 /home/bschmidt/temp/gswp3/wind_rm_gswp3_1901_2010.nc4 temp.nc
-# mv temp.nc /home/bschmidt/temp/gswp3/wind_rm_gswp3_1901_2010.nc4
-# echo 'Removed leap day from wind'
+# run from parent directory
+if [ -e settings.py ]; then
+    settings_file=settings.py 
+else 
+    settings_file=../settings.py
+fi 
 
-# nccopy -u -k 'nc4' -m 32G -c time/4018,lat/1,lon/720 /home/bschmidt/temp/gswp3/pr_rm_gswp3_1901_2010.nc4 /home/bschmidt/temp/gswp3/pr_rm_rechunked_gswp3_1901_2010.nc4
-# echo 'rechunked pr'
-# nccopy -u -k 'nc4' -m 32G -c time/4018,lat/1,lon/720 /home/bschmidt/temp/gswp3/ps_rm_gswp3_1901_2010.nc4 /home/bschmidt/temp/gswp3/ps_rm_rechunked_gswp3_1901_2010.nc4
-# echo 'rechunked ps'
-# nccopy -u -k 'nc4' -m 32G -c time/4018,lat/1,lon/720 /home/bschmidt/temp/gswp3/huss_rm_gswp3_1901_2010.nc4 /home/bschmidt/temp/gswp3/huss_rm_rechunked_gswp3_1901_2010.nc4
-# echo 'rechunked huss'
-# nccopy -u -k 'nc4' -m 32G -c time/4018,lat/1,lon/720 /home/bschmidt/temp/gswp3/rhs_rm_gswp3_1901_2010.nc4 /home/bschmidt/temp/gswp3/rhs_rm_rechunked_gswp3_1901_2010.nc4
-# echo 'rechunked rhs'
-# nccopy -u -k 'nc4' -m 32G -c time/4018,lat/1,lon/720 /home/bschmidt/temp/gswp3/rlds_rm_gswp3_1901_2010.nc4 /home/bschmidt/temp/gswp3/rlds_rm_rechunked_gswp3_1901_2010.nc4
-# echo 'rechunked rlds'
-# nccopy -u -k 'nc4' -m 32G -c time/4018,lat/1,lon/720 /home/bschmidt/temp/gswp3/rsds_rm_gswp3_1901_2010.nc4 /home/bschmidt/temp/gswp3/rsds_rm_rechunked_gswp3_1901_2010.nc4
-# echo 'rechunked rsds'
-# nccopy -u -k 'nc4' -m 32G -c time/4018,lat/1,lon/720 /home/bschmidt/temp/gswp3/tasmax_rm_gswp3_1901_2010.nc4 /home/bschmidt/temp/gswp3/tasmax_rm_rechunked_gswp3_1901_2010.nc4
-# echo 'rechunked tasmax'
-# nccopy -u -k 'nc4' -m 32G -c time/4018,lat/1,lon/720 /home/bschmidt/temp/gswp3/tasmin_rm_gswp3_1901_2010.nc4 /home/bschmidt/temp/gswp3/tasmin_rm_rechunked_gswp3_1901_2010.nc4
-# echo 'rechunked tasmin'
-# nccopy -u -k 'nc4' -m 32G -c time/4018,lat/1,lon/720 /home/bschmidt/temp/gswp3/tas_gswp3_1901_2010_noleap.nc4 /home/bschmidt/temp/gswp3/tas_rechunked_gswp3_1901_2010_noleap.nc4
-# echo 'rechunked tas'
-# nccopy -u -k 'nc4' -m 32G -c time/4018,lat/1,lon/720 /home/bschmidt/temp/gswp3/wind_rm_gswp3_1901_2010.nc4 /home/bschmidt/temp/gswp3/wind_rm_rechunked_gswp3_1901_2010.nc4
-# echo 'rechunked wind'
+# Get information from settings file
+# sed gets rid of string markers (',")
+variable="$(grep 'variable =' ${settings_file} | cut -d' ' -f3 | sed "s/'//g" | sed 's/"//g')"
+datafolder="$(grep 'data_dir =' ${settings_file} | cut -d' ' -f3 | grep $USER | sed "s/'//g" | sed 's/"//g')"
+dataset="$(grep 'dataset =' ${settings_file} | cut -d' ' -f3 | sed "s/'//g" | sed 's/"//g')"
+startyear="$(grep 'startyear =' ${settings_file} | cut -d' ' -f3 | sed "s/'//g" | sed 's/"//g')"
+endyear="$(grep 'endyear =' ${settings_file} | cut -d' ' -f3 | sed "s/'//g" | sed 's/"//g')"
+
+echo 'Rechunk the following variable'
+echo $variable
+nccopy -u -k 'nc4' -m 32G -c time/4018,lat/1,lon/720 ${datafolder}${variable}_${dataset}_${startyear}_${endyear}_noleap.nc4 \
+    ${datafolder}${variable}_rechunked_${dataset}_${startyear}_${endyear}_noleap.nc4
+echo 'rechunked' $i 'for faster access to full timeseries'
+
+# OLD LOOP OVER ALL VARIABLES ROUTINE
+# variables=( pr ps huss rhs rlds rsds tasmax tasmin tas wind )
+# echo 'Rechunk the following variables'
+# echo $variables
+# for i in "${variables[@]}"; do
+#     echo 'Rechunking' $i
+#     nccopy -u -k 'nc4' -m 32G -c time/4018,lat/1,lon/720 ${datafolder}${i}_${dataset}_1901_2010_noleap.nc4 \
+#         ${datafolder}${i}_rechunked_${dataset}_1901_2010_noleap.nc4
+#     echo 'rechunked' $i 'for faster access to full timeseries'
+# done
