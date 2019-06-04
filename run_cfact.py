5effff6c6ce276e1ccaf4e4f07e9c15f68308b2d
bschmidt <bschmidt@pik-potsdam.com>
2019-06-04 15:21:58 +0200

    Changed submission method
    implemented the same submission method as run_bayes.py has.

--- run_cfact.py
+++ run_cfact.py
@@ -16,17 +16,26 @@ import pandas as pd
 
 try:
     submitted = os.environ["SUBMITTED"] == "1"
+    task_id = int(os.environ["SLURM_ARRAY_TASK_ID"])
+    njobarray = int(os.environ["SLURM_ARRAY_TASK_COUNT"])
 except KeyError:
     submitted = False
+    njobarray = 1
+    task_id = 0
+    s.progressbar = True
 
 # get gmt file
 gmt_file = os.path.join(s.input_dir, s.gmt_file)
-gmt = nc.Dataset(gmt_file, "r")
-gmt = np.squeeze(gmt.variables["tas"][:])
+with nc.Dataset(gmt_file, "r") as gmt_obj:
+    gmt = np.squeeze(gmt_obj.variables["tas"][:])
 
 # get data to detrend
 source_file = os.path.join(s.data_dir, s.source_file)
 data = nc.Dataset(source_file, "r")
+
+latsize = data.dimensions["lat"].size
+lonsize = data.dimensions["lon"].size
+ncells = latsize * data.dimensions["lon"].size
 
 latrange = range(data.dimensions["lat"].size)
 lonrange = range(data.dimensions["lon"].size)
@@ -35,43 +44,62 @@ lonrange = range(data.dimensions["lon"].size)
 nct = data.variables["time"]
 
 tdf = bt.create_dataframe(nct, data.variables[s.variable][:, 0, 0], gmt)
+
 if not os.path.exists(s.output_dir):
     os.makedirs(s.output_dir)
     os.makedirs(Path(s.output_dir) / "cfact")
+    os.makedirs(Path(s.output_dir) / "timeseries")
 
-if __name__ == "__main__":
-    print("Variable is:")
-    print(s.variable, flush=True)
+if ncells % njobarray:
+    print("task_id", task_id)
+    print("njobarray", njobarray)
+    print("ncells", ncells)
+    raise ValueError("ncells does not fit into array job, adjust jobarray.")
 
-    cfact = cf.cfact(nct, gmt)
+calls_per_arrayjob = ncells / njobarray
 
-    if submitted:
-        with MPIPoolExecutor() as executor:
-            futures = executor.map(
-                cfact.run,
-                (cf.cfact_helper(data, i, j) for i in latrange for j in lonrange),
-            )
+# Calculate the starting and ending values for this task based
+# on the SLURM task and the number of runs per task.
+start_num = int(task_id * calls_per_arrayjob)
+end_num = int((task_id + 1) * calls_per_arrayjob - 1)
 
-    else:
-        print("serial mode")
-        futures = map(
-            cfact.run,
-            (cf.cfact_helper(data, i, j) for i in latrange for j in lonrange),
-        )
+# Print the task and run range
+print("This is SLURM task", task_id, "which will do runs", start_num, "to", end_num)
 
-    futures = list(futures)
+print("Variable is:")
+print(s.variable, flush=True)
 
-    #  create output file
-    cfact_path = os.path.join(s.output_dir, s.cfact_file)
-    cfact_file = nc.Dataset(cfact_path, "w", format="NETCDF4")
-    cfact_file.description = "beta version of counterfactual weather"
-    u.copy_nc_container(cfact_file, data)
+TIME0 = datetime.now()
 
-    k = 0
-    for i in latrange:
-        for j in lonrange:
-            cfact_file.variables[s.variable][:, i, j] = futures[k]
-            k += 1
+cfact = cf.cfact(nct, gmt)
+
+futures = []
+for n in np.arange(start_num, end_num + 1, 1, dtype=np.int):
+    i = int(n % latsize)
+    j = int(n / latsize)
+    print("This is SLURM task", task_id, "run number", n, "i,j", i, j)
+
+    data_ij = cf.cfact_helper(data, i, j)
+    futr = cfact.run(data_ij)
+    futures.append(futr)
+
+print(
+    "Estimation completed for all cells. It took {0:.1f} minutes.".format(
+        (datetime.now() - TIME0).total_seconds() / 60
+    )
+)
+
+#  create output file
+cfact_path = os.path.join(s.output_dir, s.cfact_file.split(".") + "-" + str(os.getpid()) + ".nc4")
+cfact_file = nc.Dataset(cfact_path, "w", format="NETCDF4")
+cfact_file.description = "beta version of counterfactual weather"
+u.copy_nc_container(cfact_file, data)
+
+k = 0
+for i in latrange:
+    for j in lonrange:
+        cfact_file.variables[s.variable][:, i, j] = futures[k]
+        k += 1
 
 cfact_file.close()
 #  trend_file.close()
