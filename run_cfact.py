15631ab2327f0ca005789da0cd9bf477d50b7e88
bschmidt <bschmidt@pik-potsdam.com>
2019-06-04 13:36:18 +0200

    formatting (with black)

--- run_cfact.py
+++ run_cfact.py
@@ -43,41 +43,33 @@ if __name__ == "__main__":
     print("Variable is:")
     print(s.variable, flush=True)
 
-    #  bayes = bt.bayes_regression(tdf["gmt_scaled"], s.output_dir)
     cfact = cf.cfact(nct, gmt)
-
-    #  create output file
-    cfact_path = os.path.join(s.output_dir, s.cfact_file)
-    cfact_file = nc.Dataset(cfact_path, "w", format="NETCDF4")
-    cfact_file.description = "bayesian regression test script"
-    u.copy_nc_container(cfact_file, data)
 
     if submitted:
         with MPIPoolExecutor() as executor:
             futures = executor.map(
                 cfact.run,
-                (
-                    cf.cfact_helper(data, nct, gmt, i, j)
-                    for i in latrange
-                    for j in lonrange
-                ),
+                (cf.cfact_helper(data, i, j) for i in latrange for j in lonrange),
             )
 
     else:
         print("serial mode")
         futures = map(
             cfact.run,
-            (
-                cf.cfact_helper(data, nct, gmt, i, j)
-                for i in latrange
-                for j in lonrange
-            ),
+            (cf.cfact_helper(data, i, j) for i in latrange for j in lonrange),
         )
 
     futures = list(futures)
+
+    #  create output file
+    cfact_path = os.path.join(s.output_dir, s.cfact_file)
+    cfact_file = nc.Dataset(cfact_path, "w", format="NETCDF4")
+    cfact_file.description = "beta version of counterfactual weather"
+    u.copy_nc_container(cfact_file, data)
+
     k = 0
     for i in latrange:
-        for i in lonrange:
+        for j in lonrange:
             cfact_file.variables[s.variable][:, i, j] = futures[k]
             k += 1
 
