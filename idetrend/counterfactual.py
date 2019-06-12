e117be2f36e5c997d6f29d49e83608c6645e9d36
bschmidt <bschmidt@pik-potsdam.com>
2019-06-12 12:24:09 +0200

    eliminated function
    deleted unnecessary function to create pandas data frame in the code. Data frame should be created and passed as input. This is clearer and less error prone.

--- idetrend/counterfactual.py
+++ idetrend/counterfactual.py
@@ -18,27 +18,29 @@ import settings as s
 
 
 class cfact(object):
-    def __init__(self, nct, gmt):
-        gmt_tdf = create_gmt_frame(nct, gmt)
-        self.bayes = bt.bayes_regression(gmt_tdf["gmt_scaled"], s)
-        self.gmt = gmt
-        self.nct = nct
+    def __init__(self, tdf, cfg):
+        self.bayes = bt.bayes_regression(tdf["gmt_scaled"], s)
+        self.tdf = tdf
+        self.gmt = tdf["gmt_scaled"]
+        self.trace_path = s.output_dir / "traces"
+        #  self.trace_path = (
+        #      "/p/tmp/mengel/isimip/isi-cfact/output/isicfat_gswp3_tas_full/traces/trace_"
+        #      + str(i)
+        #      + "_"
+        #      + str(j)
+        #  )
 
     def get_data(self, data, i, j):
-        self.trace_path = (
-            "/p/tmp/mengel/isimip/isi-cfact/output/isicfat_gswp3_tas_full/traces/trace_"
-            + str(i)
-            + "_"
-            + str(j)
-        )
         #  self.trace_path = os.path.join(s.output_dir, "traces", "trace_" + str(i) + "_" + str(j))
-        self.tdf = bt.create_dataframe(self.nct, data, self.gmt)
+        self.tdf["y_scaled"] = bt.y_norm(data, data)
+        self.tdf["y"] = data
         self.model, self.x_data = self.bayes.setup_model(self.tdf)
         print("This is process", os.getpid(), "working on", self.trace_path)
 
-    def load_trace(self):
-        with self.model:
-            self.trace = pm.load_trace(self.trace_path)
+    def load_trace(self, i, j):
+        #  with self.bayes.model:
+        path = self.trace_path / ("trace_" + str(i) + "_" + str(j))
+        self.trace = pm.load_trace(path, model=self.bayes.model)
 
     def det_post(self):
         x_yearly, x_trend = self.x_data
@@ -79,25 +81,25 @@ class cfact(object):
         #  if not os.path.exists(cfact_path_ts):
         #      os.makedirs(cfact_path)
 
-        try:
-            self.get_data(data, i, j)
-            self.load_trace()
-            trend_post, year_trend_post, post = self.det_post()
-            # FIXME: This is already done by y_inv() on different parts in det_post
-            #  post = u.y_inv(self.post, self.tdf["y"])
-            self.det_cfact(trend_post, year_trend_post)
-            self.save_ts(data, i, j, cfact_path_ts)
-            #  self.plot_cfact_ts(post, i, j, 40177)
-            print("Calc succesfull")
-            #  return self.cfact
-            return "Done"
+        #  try:
+        self.get_data(data, i, j)
+        self.load_trace(i, j)
+        trend_post, year_trend_post, post = self.det_post()
+        # FIXME: This is already done by y_inv() on different parts in det_post
+        #  post = u.y_inv(self.post, self.tdf["y"])
+        self.det_cfact(trend_post, year_trend_post)
+        self.save_ts(data, i, j, cfact_path_ts)
+        #  self.plot_cfact_ts(post, i, j, 40177)
+        print("Calc succesfull")
+        #  return self.cfact
+        return "Done"
 
-        except:
-            empty = np.empty((data.shape[0],))
-            empty[:] = np.nan
-            print("trace missing! Printing nans!")
-            self.save_ts(empty, i, j, cfact_path_ts)
-            return np.nan
+        #  except:
+        #      empty = np.empty((data.shape[0],))
+        #      empty[:] = np.nan
+        #      print("trace missing! Printing nans!")
+        #      self.save_ts(empty, i, j, cfact_path_ts)
+        #      return np.nan
 
     def plot_cfact_ts(self, post, i, j, last):
         import matplotlib.dates as mdates
@@ -191,7 +193,7 @@ class cfact(object):
         latitudes[:] = (180 - i)/2 -.25
         longitudes[:] = (j -360)/2 + .25
         times[:] = range(data.shape[0])
-        data_ts[:] = data
+        data_ts[:] = np.array(data)
         cfact_file_ts.close()
 
 
@@ -203,13 +205,3 @@ def cfact_helper(data_to_detrend, i, j):
     data = data_to_detrend.variables[s.variable][:, i, j]
     return (data, i, j)
 
-
-def create_gmt_frame(nct, gmt):
-    ds = pd.to_datetime(
-        nct[:], unit="D", origin=pd.Timestamp(nct.units.lstrip("days since"))
-    )
-    t_scaled = (ds - ds.min()) / (ds.max() - ds.min())
-    gmt_on_data_cal = np.interp(t_scaled, np.linspace(0, 1, len(gmt)), gmt)
-    gmt_scaled = bt.y_norm(gmt_on_data_cal, gmt_on_data_cal)
-    gmt_tdf = pd.DataFrame({"gmt_scaled": gmt_scaled})
-    return gmt_tdf
