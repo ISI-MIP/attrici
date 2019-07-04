6a72179de25fa55f1dcb4dac7b6d6a1b0c1c2908
bschmidt <bschmidt@pik-potsdam.de>
2019-07-04 14:39:00 +0200

    easier reading and complete output
    changed the algorithm to use loops to create all output variables scaled and rescaled. A single netCDF4 file is created for each variable

--- merge_cfact.py
+++ merge_cfact.py
@@ -10,6 +10,7 @@ from pathlib import Path
 
 import settings as s
 import netCDF4 as nc
+import pandas as pd
 import numpy as np
 
 
@@ -43,43 +44,61 @@ def form_global_nc(ds, time, lat, lon):
     times[:] = time
 
 
-data_list = glob.glob(str(s.output_dir) + "/timeseries/" + s.variable + "*.nc4")
-
+#  data_gen = Path(s.output_dir / "timeseries").glob("**/*.csv")
+data_gen = Path("/p/tmp/mengel/isimip/isi-cfact/output/isicf004_gswp3_tas_2p5deg/timeseries").glob("**/*.csv")
+data_list = []
+lat = []
+lon = []
 lat_indices = []
-for string in data_list:
-    i = string.split("_")[-2]
-    lat_indices.append(i)
-
 lon_indices = []
-for string in data_list:
-    j = string.split("_")[-1].split(".")[0]
-    lon_indices.append(j)
+for i in data_gen:
+    data_list.append(str(i))
+    lat_float = float(str(i).split("lat")[-1].split("_")[0])
+    lon_float = float(str(i).split("lon")[-1].split(".csv")[0])
+    lat.append(lat_float)
+    lon.append(lon_float)
+    lat_indices.append(int(180 - 2*lat_float - .5)/s.subset)
+    lon_indices.append(int(2*lon_float - .5 + 360)/s.subset)
 
-print(lon_indices)
 
 obs = nc.Dataset(Path(s.input_dir) / s.source_file, "r")
 time = obs.variables["time"][:]
 lat = obs.variables["lat"][:]
 lon = obs.variables["lon"][:]
-cfact = nc.Dataset(Path(s.output_dir) / "cfact" / s.cfact_file, "w", format="NETCDF4")
-trend = nc.Dataset(Path(s.output_dir) / "cfact" / s.trend_file, "w", format="NETCDF4")
-form_global_nc(cfact, time, lat, lon)
-form_global_nc(trend, time, lat, lon)
+headers = pd.read_csv(data_list[0], index_col=0, nrows=1).keys()
+headers = headers.drop(["ds", "gmt", "gmt_scaled"])
+
+outs = []
+for head in headers:
+    outs.append(nc.Dataset(Path(s.output_dir)
+                           / "cfact"
+                           / (head + "_" + s.cfact_file.split("_", 1)[1]),
+                           "w",
+                           format="NETCDF4"))
+for out in outs:
+    form_global_nc(out, time, lat, lon)
+#  trend = nc.Dataset(Path(s.output_dir) / "cfact" / s.trend_file, "w", format="NETCDF4")
+#  estimate = nc.Dataset(Path(s.output_dir) / "cfact" / s.est_file, "w", format="NETCDF4")
+#  form_global_nc(cfact, time, lat, lon)
+#  form_global_nc(trend, time, lat, lon)
+#  form_global_nc(estimate, time, lat, lon)
 
 for (i, j, path) in it.zip_longest(lat_indices, lon_indices, data_list):
-    print(i)
-    print(j)
     print(path)
+    print(i, j)
+    df = pd.read_csv(path, index_col=0, engine='c')
+    obs_ts = np.array(obs.variables[s.variable][:, i, j])
+    for (out, head) in it.zip_longest(outs, headers):
+        ts = np.array(df[head])
+        out.variables[s.variable][:, i, j] = ts
+    #  ts = np.array(pd.read_csv(path)["cfact"])
+    #  cfact.variables[s.variable][:, i, j] = ts
+    #  trend.variables[s.variable][:, i, j] = obs_ts - ts
+    #  estimate.variables[s.variable][:, i, j] = np.array(pd.read_csv(path)["estimated"])
 
-    with nc.Dataset(path, "r") as nc_ts:
-
-        obs_ts = obs.variables[s.variable][:, i, j]
-        ts = nc_ts.variables[s.variable][:].data
-        cfact.variables[s.variable][:, i, j] = ts
-        trend.variables[s.variable][:, i, j] = obs_ts - ts
-        print(i, j)
-
-print("hello")
-obs.close()
-cfact.close()
-trend.close()
+for out in outs:
+    out.close()
+#  obs.close()
+#  cfact.close()
+#  trend.close()
+#  estimate.close()
