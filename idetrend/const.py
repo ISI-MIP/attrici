9f6889ae476f08e9207a6a237e5f47cdfde904f8
bschmidt <bschmidt@pik-potsdam.de>
2019-07-08 17:19:02 +0200

    changed transformation and scaling
    changed scaling and transformation methods to work with dict's
    transform_dict and retransform_dict. Each links a variable specific
    function, calles from bayes_detrending.py

--- idetrend/const.py
+++ idetrend/const.py
@@ -5,55 +5,56 @@ def standard(y_to_scale, y_orig):
 
 def precip(y_to_scale, y_orig):
     """scale and transform data with lower boundary y to y_original"""
-    y = np.exp(y)
+    y_to_scale[y_to_scale<=0.000001157407] = 0   # amounts to .1 mm per day if unit is mm per sec
+    y_to_scale = np.log(y_to_scale)
     return (y_to_scale - y_orig.min()) / (y_orig.max() - y_orig.min())
 
-def logit(y_to_scale, y_orig):
+def rhs(y_to_scale, y_orig):
     """scale and transform data with lower boundary y to y_original"""
-    y = 2.0 * np.ma.arctanh(2.0 * data / (maxval - minval) - 1.0)
+    y_to_scale = 2.0 * np.ma.arctanh(2.0 * y_to_scale / (100 - 0) - 1.0)
     return (y_to_scale - y_orig.min()) / (y_orig.max() - y_orig.min())
 
-def expit(data):
-    """ scaled inverse logit for input data of values in [0, 100]
-    as for rhs. minval and maxval differ by purpose from these
-    in dictionaries below."""
-    minval = 0
-    maxval = 100
-    return minval + (maxval - minval) * 0.5 * (1.0 + np.ma.tanh(0.5 * data))
+def wind(y_to_scale, y_orig):
+    """scale and transform wind data with lower boundary y to y_original"""
+    y_to_scale = np.log(y_to_scale)
+    return (y_to_scale - y_orig.min()) / (y_orig.max() - y_orig.min())
 
 transform_dict = {
     "tasmin": standard,
     "tas": standard,
     "tasmax": standard,
     "pr": precip,
-    "rhs": ,
-    "ps": None,
-    "rsds": None,
-    "rlds": None,
-    "wind": [np.ma.log, np.ma.exp],
+    "rhs": rhs,
+    "ps": standard,
+    "rsds": standard,
+    "rlds": standard,
+    "wind": wind,
 }
 
 def rescale(y, y_orig):
     """rescale "standard" data y to y_original"""
-    return y * (y_orig.max() - y_orig.min()) + y_orig.min()
+    return y * (y_orig.max() - y_orig.min())
 
 def re_standard(y, y_orig):
     """standard" data y to y_original"""
-    return y * (y_orig.max() - y_orig.min())
+    return y * (y_orig.max() - y_orig.min()) + y_orig.min()
 
 def re_precip(y, y_orig):
     """rescale and transform data with lower boundary y to y_original"""
-    y[y<=0.000001157407] = 0   # amounts to .1 mm per day if unit is mm per sec
-    y = np.log(y)
+    y = np.exp(y)
     return y * (y_orig.max() - y_orig.min()) + y_orig.min()
 
-def logit(data):
-    """ scaled logit for input data of values in [0, 100]
+def re_rhs(y, y_orig):
+    """ scaled inverse logit for input data of values in [0, 100]
     as for rhs. minval and maxval differ by purpose from these
-    in dictionaries below. """
-    minval = 0
-    maxval = 100
-    return 2.0 * np.ma.arctanh(2.0 * (data - minval) / (maxval - minval) - 1.0)
+    in dictionaries below."""
+    y = 100 * 0.5 * (1.0 + np.ma.tanh(0.5 * y))
+    return y * (y_orig.max() - y_orig.min()) + y_orig.min()
+
+def re_wind(y, y_orig):
+    """rescale and transform data with lower boundary y to y_original"""
+    y = np.exp(y)
+    return y * (y_orig.max() - y_orig.min()) + y_orig.min()
 
 #  set of inverse transform functions
 retransform_dict = {
@@ -61,61 +62,34 @@ retransform_dict = {
     "tas": re_standard,
     "tasmax": re_standard,
     "pr": re_precip,
-    "rhs": [logit, expit],
-    "ps": None,
-    "rsds": None,
-    "rlds": None,
-    "wind": [np.ma.log, np.ma.exp],
+    "rhs": re_rhs,
+    "ps": re_standard,
+    "rsds": re_standard,
+    "rlds": re_standard,
+    "wind": re_wind,
 }
 
-
-
-# transformations come in tuples
-# [transform, inverse_transform]
-transform = {
-    "tasmin": None,
-    "tas": None,
-    "tasmax": None,
-    "pr": [np.ma.log, np.ma.exp],
-    "rhs": [logit, expit],
-    "ps": None,
-    "rsds": None,
-    "rlds": None,
-    "wind": [np.ma.log, np.ma.exp],
-}
-
-unit = {
-    "tasmax": "K",
-    "tas": "K",
-    "tasmin": "K",
-    "pr": "mm/s",
-    "rhs": "%",
-    "ps": "hPa",
-    "rsds": u"J/cm\u00B2",
-    "rlds": u"J/cm\u00B2",
-    "wind": "m/s",
-}
-
-minval = {
-    "tasmax": None,
-    "tas": None,
-    "tasmin": None,
-    "pr": 0.000001157407,  # amounts to .1 mm per day if unit is mm per sec
-    "rhs": 0.01,
-    "ps": None,
-    "rsds": 0.0,
-    "rlds": 0.0,
-    "wind": 0.0,
-}
-
-maxval = {
-    "tasmax": None,
-    "tas": None,
-    "tasmin": None,
-    "pr": None,
-    "rhs": 99.9,
-    "ps": None,
-    "rsds": 3025.0,
-    "rlds": 3025.0,
-    "wind": None,
-}
+######## Not needed but kept for possible later use ####
+#  unit = {
+#      "tasmax": "K",
+#      "tas": "K",
+#      "tasmin": "K",
+#      "pr": "mm/s",
+#      "rhs": "%",
+#      "ps": "hPa",
+#      "rsds": u"J/cm\u00B2",
+#      "rlds": u"J/cm\u00B2",
+#      "wind": "m/s",
+#  }
+#
+#  maxval = {
+#      "tasmax": None,
+#      "tas": None,
+#      "tasmin": None,
+#      "pr": None,
+#      "rhs": 99.9,
+#      "ps": None,
+#      "rsds": 3025.0,
+#      "rlds": 3025.0,
+#      "wind": None,
+#  }
