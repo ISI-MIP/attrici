ff39e397ca8e3eec0d07e60f4b80a708bc3f10a5
bschmidt <bschmidt@pik-potsdam.de>
2019-07-09 14:26:56 +0200

    load recent modules

--- preprocessing/rechunk.sh
+++ preprocessing/rechunk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --qos=priority
-##SBATCH --partition=priority
+#SBATCH --partition=priority
 #SBATCH --job-name=rechunk
 #SBATCH --account=isipedia
 #SBATCH --output=../output/rechunk.out
@@ -13,8 +13,10 @@
 #SBATCH --mem=60000
 #SBATCH --exclusive
 #
-module load intel/2018.1
+module load intel/2019.4
+module load netcdf-c/4.6.1/intel/parallel
 module load nco/4.7.8
+
 if [ -e settings.py ]; then
     settings_file=settings.py 
 else 
@@ -37,6 +39,6 @@ outputfile=${datafolder}/input/${variable}_${dataset}.nc4
 # echo $variable
 echo 'Inputfile:' ${inputfile}
 echo 'Outputfile:' ${outputfile}
-# nccopy -u -k 'nc4' -m 64G -c time/4018,lat/1,lon/1 ${inputfile} ${outputfile}
-nccopy -w -k 'nc4' -c time/4018,lat/1,lon/720 ${inputfile} ${outputfile} 
+nccopy -w -k 'nc4' -c time/40177,lat/1,lon/1 /p/tmp/bschmidt/gswp3/pr_gswp3.nc4 /p/tmp/bschmidt/gswp3/input/pr_gswp3.nc4
+# nccopy -w -k 'nc4' -c time/4018,lat/1,lon/1 ${inputfile} ${outputfile}
 # echo 'rechunked' $variable 'for faster access to full timeseries'
