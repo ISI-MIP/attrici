04decc5c6a87a9ad05ff0a8e524a34d7a2d1460b
bschmidt <bschmidt@pik-potsdam.de>
2019-07-10 13:40:55 +0200

    changed np methods to functions
    changed numpy methods to functions in order to ignore np.nans in
    calculations and avoid getting "nan" outputs.

--- idetrend/const.py
+++ idetrend/const.py
@@ -1,26 +1,25 @@
 import numpy as np
 
 def standard(y_to_scale, y_orig):
-    return (y_to_scale - y_orig.min()) / (y_orig.max() - y_orig.min())
+    return (y_to_scale - np.nanmin(y_orig)) / (np.nanmax(y_orig) - np.nanmin(y_orig))
 
 def precip(y_to_scale, y_orig):
     """scale and transform data with lower boundary y to y_original"""
     y_to_scale[y_to_scale<=0.000001157407] = 0   # amounts to .1 mm per day if unit is mm per sec
-    y_to_scale = np.log(y_to_scale)
-    return (y_to_scale - y_orig.min()) / (y_orig.max() - y_orig.min())
+    return np.log(y_to_scale)
 
 def rhs(y_to_scale, y_orig):
     """scale and transform data with lower boundary y to y_original"""
     y_to_scale = 2.0 * np.ma.arctanh(2.0 * y_to_scale / (100 - 0) - 1.0)
-    return (y_to_scale - y_orig.min()) / (y_orig.max() - y_orig.min())
+    return (y_to_scale - np.nanmin(y_orig)) / (np.nanmax(y_orig) - np.nanmin(y_orig))
 
 def wind(y_to_scale, y_orig):
     """scale and transform wind data with lower boundary y to y_original"""
     y_to_scale = np.log(y_to_scale)
-    return (y_to_scale - y_orig.min()) / (y_orig.max() - y_orig.min())
+    return (y_to_scale - np.nanmin(y_orig)) / (np.nanmax(y_orig) - np.nanmin(y_orig))
 
 transform_dict = {
-    "tasmin": standard,
+    "tasmin": None,
     "tas": standard,
     "tasmax": standard,
     "pr": precip,
@@ -33,27 +32,27 @@ transform_dict = {
 
 def rescale(y, y_orig):
     """rescale "standard" data y to y_original"""
-    return y * (y_orig.max() - y_orig.min())
+    return y * (np.nanmax(y_orig) - np.nanmin(y_orig))
 
 def re_standard(y, y_orig):
     """standard" data y to y_original"""
-    return y * (y_orig.max() - y_orig.min()) + y_orig.min()
+    return y * (np.nanmax(y_orig) - np.nanmin(y_orig)) + np.nanmin(y_orig)
 
 def re_precip(y, y_orig):
     """rescale and transform data with lower boundary y to y_original"""
-    y = y * (y_orig.max() - y_orig.min()) + y_orig.min()
+    y = y * (np.nanmax(y_orig) - np.nanmin(y_orig)) + np.nanmin(y_orig)
     return np.exp(y)
 
 def re_rhs(y, y_orig):
     """ scaled inverse logit for input data of values in [0, 100]
     as for rhs. minval and maxval differ by purpose from these
     in dictionaries below."""
-    y = y * (y_orig.max() - y_orig.min()) + y_orig.min()
+    y = y * (np.nanmax(y_orig) - np.nanmin(y_orig)) + np.nanmin(y_orig)
     return 100 * 0.5 * (1.0 + np.ma.tanh(0.5 * y))
 
 def re_wind(y, y_orig):
     """rescale and transform data with lower boundary y to y_original"""
-    y = y * (y_orig.max() - y_orig.min()) + y_orig.min()
+    y = y * (np.nanmax(y_orig) - np.nanmin(y_orig)) + np.nanmin(y_orig)
     return np.exp(y)
 
 #  set of inverse transform functions
