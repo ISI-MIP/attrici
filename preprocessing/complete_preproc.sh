1707b59dfbd962fb0d54fea0a86a47e12dcdbfbe
bschmidt <bschmidt@pik-potsdam.de>
2019-07-11 23:27:12 +0200

    changed slurm options

--- preprocessing/complete_preproc.sh
+++ preprocessing/complete_preproc.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 #SBATCH --qos=priority
-#SBATCH --partition=ram_gpu
+#SBATCH --partition=priority
 #SBATCH --job-name=complete_pre
 #SBATCH --account=isipedia
 #SBATCH --output=../output/%x.out
 #SBATCH --error=../output/%x.err
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=bschmidt@pik-potsdam.de
-#SBATCH --time=00-2:50:00
+#SBATCH --time=00-23:50:00
 
 # # block one node completely to get all its memory.
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-##SBATCH --mem=60000
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=60000
 #SBATCH --exclusive
 
 module purge
@@ -32,7 +33,9 @@ echo "\nPython interpreter used is:"
 which python
 echo "\n-----\n"
 
-# srun yes | bash merge_data.sh
-srun /home/bschmidt/.conda/envs/mpi_py3/bin/python rechunk.py
-# srun python subset.py
+srun yes | bash merge_data.sh
+srun bash rechunk.sh
+# srun /home/bschmidt/.conda/envs/mpi_py3/bin/python rechunk.py
+# srun bash ncks_subset.sh
+srun python subset.py
 # python3 calc_gmt_by_ssa.py
