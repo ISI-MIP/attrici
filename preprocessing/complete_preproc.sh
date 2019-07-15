3ca55bfae9381ab541967405113d355aeeb36e88
bschmidt <bschmidt@pik-potsdam.de>
2019-07-15 18:09:58 +0200

    switched all scripts called by this one to shell
    removed python-related code, since all preprocessing scripts (except calculation of gmt, which should be called directly) are now shell scripts

--- preprocessing/complete_preproc.sh
+++ preprocessing/complete_preproc.sh
@@ -13,27 +13,23 @@
 # # block one node completely to get all its memory.
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=16
+##SBATCH --cpus-per-task=16
 #SBATCH --mem=60000
 #SBATCH --exclusive
 
 module purge
-module load anaconda/5.0.0_py3
+# module load anaconda/5.0.0_py3
 module load intel/2018.1
-module load netcdf-c/4.6.1/intel/parallel
+module load netcdf-c/4.6.1/intel/serial
 module load nco/4.7.8
 
-# Run complete preprocessing routine
-echo "-----\n"
-if [ "$USER" == "bschmidt" ]; then
-    echo "\nUser is\n" $USER
-    source activate mpi_py3
-fi
-echo "\nPython interpreter used is:"
-which python
-echo "\n-----\n"
-
-srun yes | bash merge_data.sh
-srun bash rechunk.sh
-srun bash ncks_subset.sh
+echo "Begin merging!"
+yes | bash merge_data.sh
+echo "Done merging!"
+echo "Begin rechunking!"
+bash rechunk.sh
+echo "Done rechunking!"
+echo "Begin subsetting!"
+bash subset.sh
+echo "Done subsetting!"
 # python3 calc_gmt_by_ssa.py
