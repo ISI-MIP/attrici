cec3f4523f25504a551e4c1f3615544eb3db09e0
bschmidt <bschmidt@pik-potsdam.de>
2019-07-16 17:07:36 +0200

    handling of subset data
    fixed an error, where lonlat indexes of subsets would not be calculated correctly

--- merge_cfact.py
+++ merge_cfact.py
@@ -43,40 +43,43 @@ def form_global_nc(ds, time, lat, lon, vnames, torigin):
     times[:] = time
 
 
+#  get input and output
 data_gen = Path(s.output_dir / "timeseries").glob("**/*.csv")
-data_list = []
-lat = []
-lon = []
-lat_indices = []
-lon_indices = []
-for i in data_gen:
-    data_list.append(str(i))
-    lat_float = float(str(i).split("lat")[-1].split("_")[0])
-    lon_float = float(str(i).split("lon")[-1].split(".csv")[0])
-    lat.append(lat_float)
-    lon.append(lon_float)
-    lat_indices.append(int(180 - 2 * lat_float - 0.5) / s.subset)
-    lon_indices.append(int(2 * lon_float - 0.5 + 360) / s.subset)
-
-
-obs = nc.Dataset(Path(s.input_dir) / s.source_file, "r")
-time = obs.variables["time"][:]
-lat = obs.variables["lat"][:]
-lon = obs.variables["lon"][:]
-headers = pd.read_csv(data_list[0], index_col=0, nrows=1).keys()
-headers = headers.drop(["y", "y_scaled", "t", "ds", "gmt", "gmt_scaled"])
-
 out = nc.Dataset(Path(s.output_dir)
                  / "cfact"
                  / s.cfact_file,
                  "w",
                  format="NETCDF4",
                  )
+
+
+data_list = []
+lat_indices = []
+lon_indices = []
+for i in data_gen:
+    data_list.append(str(i))
+    lat_float = float(str(i).split("lat")[-1].split("_")[0])
+    lon_float = float(str(i).split("lon")[-1].split(".csv")[0])
+    lat_indices.append(int(180 - 2 * lat_float - 0.5))
+    lon_indices.append(int(2 * lon_float - 0.5 + 360))
+
+obs = nc.Dataset(Path(s.input_dir) / s.source_file, "r")
+time = obs.variables["time"][:]
+lat = obs.variables["lat"][:]
+lon = obs.variables["lon"][:]
+
+#  get headers and form empty netCDF file with all meatdata
+headers = pd.read_csv(data_list[0], index_col=0, nrows=1).keys()
+headers = headers.drop(["y", "y_scaled", "t", "ds", "gmt", "gmt_scaled"])
 form_global_nc(out, time, lat, lon, headers, obs.variables["time"].units)
+
+#  adjust indices if datasets are subsets (lat/lon-shapes are smaller than 360/720)
+lat_indices *= int(lat.shape[0]/360)
+lon_indices *= int(lon.shape[0]/720)
 
 for (i, j, path) in it.zip_longest(lat_indices, lon_indices, data_list):
     print(path)
-    print(i, j)
+    print("indices are:", i, j)
     df = pd.read_csv(path, index_col=0, engine="c")
     # obs_ts = obs.variables[s.variable][:, i, j]
     # print(np.sum(np.isnan(obs_ts)))
