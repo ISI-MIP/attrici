695aaa6e5cdb2ab62fdb9e4295fe555634f5acd2
Matthias Mengel <mengel@pik-potsdam.de>
2019-08-07 10:05:33 +0200

    only reshuffle.

--- idetrend/const.py
+++ idetrend/const.py
@@ -15,6 +15,31 @@ threshold = {
 }
 
 
+def fourier_series(t, p, modes):
+    # 2 pi n / p
+    x = 2 * np.pi * np.arange(1, modes + 1) / p
+    # 2 pi n / p * t
+    x = x * t[:, None]
+    x = np.concatenate((np.cos(x), np.sin(x)), axis=1)
+    return x
+
+
+def rescale_fourier(df, modes):
+    """ This function computes a scaled (0, 1) fourier series for a given input dataset.
+    An input vector of dates ("ds") must be available in a datestamp format.
+    If the time vector has gaps (due to dropped NA's), the fourier series will
+     also contain gaps (jumps in value).
+    The output format will be of [len["ds"], 2*modes], where the first
+    half of the columns contains the cos(x)-series and die latter half
+    contains the sin(x)-series
+    """
+
+    # rescale the period, as t is also scaled
+    p = 365.25 / (df["ds"].max() - df["ds"].min()).days
+    x = fourier_series(df["t"], p, modes)
+    return x
+
+
 # def scale(y_to_scale, y_orig, gmt=False):
 #     if gmt:
 #         lower = 0
@@ -111,30 +136,6 @@ threshold = {
 #     "wind": re_wind,
 # }
 
-
-def fourier_series(t, p, modes):
-    # 2 pi n / p
-    x = 2 * np.pi * np.arange(1, modes + 1) / p
-    # 2 pi n / p * t
-    x = x * t[:, None]
-    x = np.concatenate((np.cos(x), np.sin(x)), axis=1)
-    return x
-
-
-def rescale_fourier(df, modes):
-    """ This function computes a scaled (0, 1) fourier series for a given input dataset.
-    An input vector of dates ("ds") must be available in a datestamp format.
-    If the time vector has gaps (due to dropped NA's), the fourier series will
-     also contain gaps (jumps in value).
-    The output format will be of [len["ds"], 2*modes], where the first
-    half of the columns contains the cos(x)-series and die latter half
-    contains the sin(x)-series
-    """
-
-    # rescale the period, as t is also scaled
-    p = 365.25 / (df["ds"].max() - df["ds"].min()).days
-    x = fourier_series(df["t"], p, modes)
-    return x
 
 
 ######## Not needed but kept for possible later use ####
