323af5d0e76bcf83649d1d6c4f2c78674a41d6a1
Simon Treu <sitreu@pik-potsdam.de>
2019-11-21 10:56:10 +0100

    add cartopy for plotting coastlines

--- plotting/plot_timeave_maps.py
+++ plotting/plot_timeave_maps.py
@@ -1,9 +1,12 @@
 import matplotlib.pylab as plt
+import cartopy.crs as ccrs
 import numpy as np
 import netCDF4 as nc
 from pathlib import Path
-
 plt.rcParams["figure.figsize"] = 12,14
+import os
+
+os.environ['PROJ_LIB'] = '/home/sitreu/anaconda/share/proj'
 
 # data_dir = Path("/p/tmp/mengel/isimip/isi-cfact/output")
 data_dir = Path("/home/sitreu/Documents/PIK/Counter Factuals/isi-cfact/output")
@@ -20,17 +23,24 @@ vmax=4e-5
 vmin=-vmax
 lati = 8
 loni = 4
-fig, ax = plt.subplots(nrows=2,ncols=1)
 # y are the original observations, cfact the counterfactual
 for i,case in enumerate(["y", "cfact"]):
     data = ncd.variables[case][:]
     # last minus first 30 years
     trend = (data[-30 * 12:, ::sb, ::sb].mean(axis=0) -
              data[0:30 * 12:, ::sb, ::sb].mean(axis=0))
-    img = ax[i].imshow(trend,vmin=vmin,vmax=vmax)
-    plt.colorbar(img, ax=ax[i], shrink=0.6)
-    ax[i].grid()
-    ax[i].plot(loni, lati, "x", markersize=20, markeredgewidth=3, color="r",)
+    # -+5 in the extent account for she cell size of 10
+    ax = plt.subplot(211+i, projection=ccrs.PlateCarree(central_longitude=0.0))
+    ax.coastlines()
+    img = ax.imshow(trend,vmin=vmin,vmax=vmax,
+                       extent=[ncd.variables['lon'][:].min() - 5,
+                               ncd.variables['lon'][:].max() + 5,
+                               ncd.variables['lat'][:].min() - 5,
+                               ncd.variables['lat'][:].max() + 5])
+    plt.colorbar(img, ax=ax, shrink=0.6)
+    ax.grid()
+    ax.plot(loni, lati, "x", markersize=20, markeredgewidth=3, color="r",)
+    plt.title(case)
 
 plt.tight_layout()
 plt.savefig(figure_dir/"trend_map.jpg",dpi=80)
