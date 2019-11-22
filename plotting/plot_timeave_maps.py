83155d027723f81194749361bc1cca29fa40a876
Simon Treu <sitreu@pik-potsdam.de>
2019-11-22 14:05:47 +0100

    plotting maps now. Data loading is same as before

--- plotting/plot_timeave_maps.py
+++ plotting/plot_timeave_maps.py
@@ -1,44 +1,60 @@
+import argparse
 import matplotlib.pylab as plt
 import cartopy.crs as ccrs
 import numpy as np
 import netCDF4 as nc
 from pathlib import Path
 plt.rcParams["figure.figsize"] = 12,14
-import os
 
-os.environ['PROJ_LIB'] = '/home/sitreu/anaconda/share/proj'
 
-# data_dir = Path("/p/tmp/mengel/isimip/isi-cfact/output")
-data_dir = Path("/home/sitreu/Documents/PIK/Counter Factuals/isi-cfact/output")
+def get_path(data_dir, var, dataset, runid):
+    return data_dir/Path(runid)/"cfact"/var/Path(
+        var+"_"+dataset.upper()+"_cfactual_monmean.nc4")
 
-figure_dir = data_dir/"figures"/"maps"
-figure_dir.mkdir(parents=True, exist_ok=True)
-print(figure_dir)
 
-ncd = nc.Dataset(data_dir/"pr_GSWP3_cfactual_monmean.nc4","r")
+def main(runid="isicf014_gswp3_pr_flexmode_1111", variable="pr", dataset="gswp3"):
+    # data_dir = Path("/p/tmp/mengel/isimip/isi-cfact/output")
+    data_dir = Path("/home/sitreu/Documents/PIK/Counter Factuals/isi-cfact/output")
 
-# Plotting
-vmax=1e-5
-vmin=-vmax
-lati = 8
-loni = 4
-# y are the original observations, cfact the counterfactual
-for i,case in enumerate(["y", "cfact"]):
-    data = ncd.variables[case][:]
-    # last minus first 30 years
-    trend = (data[-30 * 12:, ::-1, ::1].mean(axis=0) -
-             data[0:30 * 12:, ::-1, ::1].mean(axis=0))
+    figure_dir = data_dir / "figures" / runid / "maps"
+    figure_dir.mkdir(parents=True, exist_ok=True)
+    print(figure_dir)
 
-    ax = plt.subplot(211+i, projection=ccrs.PlateCarree(central_longitude=0.0))
-    ax.coastlines()
-    img = ax.imshow(trend
-                    , vmin=vmin, vmax=vmax
-                    , extent=[-180, 180, -90, 90]
-                    , cmap='RdBu')
-    plt.colorbar(img, ax=ax, shrink=0.6)
-    ax.grid()
-    ax.plot(loni, lati, "x", markersize=20, markeredgewidth=3, color="r",)
-    plt.title(case)
+    ncd = nc.Dataset(get_path(data_dir, variable, dataset, runid),"r")
 
-plt.tight_layout()
-plt.savefig(figure_dir/"trend_map.jpg",dpi=80)
+    # Plotting
+    vmax=1e-5
+    vmin=-vmax
+    lati = 8
+    loni = 4
+    # y are the original observations, cfact the counterfactual
+    for i,case in enumerate(["y", "cfact"]):
+        data = ncd.variables[case][:]
+        # last minus first 30 years
+        trend = (data[-30 * 12:, ::-1, ::1].mean(axis=0) -
+                 data[0:30 * 12:, ::-1, ::1].mean(axis=0))
+
+        ax = plt.subplot(211+i, projection=ccrs.PlateCarree(central_longitude=0.0))
+        ax.coastlines()
+        img = ax.imshow(trend
+                        , vmin=vmin, vmax=vmax
+                        , extent=[-180, 180, -90, 90]
+                        , cmap='RdBu')
+        plt.colorbar(img, ax=ax, shrink=0.6)
+        ax.grid()
+        ax.plot(loni, lati, "x", markersize=20, markeredgewidth=3, color="r",)
+        plt.title(case)
+
+    plt.tight_layout()
+    plt.savefig(figure_dir/"trend_map.jpg",dpi=80)
+
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('runid', nargs='*', help='provide name of the experiment.')
+    o = parser.parse_args()
+    try:
+        main(runid=o.runid[0])
+    except IndexError:
+        print('no runid provided')
+        main()
