ae9bd68038d6e38c359be2bc72d7ca7288cebd1f
Simon Treu <sitreu@pik-potsdam.de>
2020-01-13 16:20:58 +0100

    Broke TasCycle 2 commits before this one. Now bring it back

--- icounter/models.py
+++ icounter/models.py
@@ -242,42 +242,51 @@ class TasLongterm(icounter.distributions.Normal, Tas):
         return model
 
 
-class TasLongterm(icounter.distributions.Normal, Tas):
+class TasCycle(icounter.distributions.Normal, Tas):
 
     """ Influence of GMT is modelled through a shift of
     mu and sigma parameters in a Normal distribution.
     """
     def __init__(self, modes):
-        super(TasLongterm, self).__init__()
+        super(TasCycle, self).__init__()
         self.modes = modes
+        self.test = False
 
     def setup(self, df_subset):
 
         model = pm.Model()
 
         with model:
+            # FIXME: We can assume that all tas values are valid i think,
+            # so use df_subset directly.
             df_valid = df_subset.dropna(axis=0, how="any")
             gmtv = pm.Data("gmt", df_valid["gmt_scaled"].values)
+            xf0 = pm.Data("xf0", df_valid.filter(like="mode_0_").values)
 
             # b_mu is in the interval (-inf,inf)
             b_mu = pm.Normal("b_mu", mu=0.5, sigma=1)
             # a_mu in (-inf, inf)
             a_mu = pm.Normal("a_mu", mu=0, sigma=1)
+
+            yearly_mu = pm.Normal("yearly_mu", mu=0.0, sd=1.0, shape=xf0.dshape[1])
             # in (-inf, inf)
-            mu = pm.Deterministic("mu", a_mu * gmtv + b_mu)
+            mu = pm.Deterministic("mu", a_mu * gmtv + b_mu + det_dot(xf0, yearly_mu))
 
             # should be same for b and a, so that a is symmetric around zero
             lam = 1
             # b_sigma is in the interval (0,inf)
             b_sigma = pm.Exponential("b_sigma", lam=lam)
-            # a_sigma is in the interval (-b, inf), mode at 0
+            yearly_sigma = pm.Exponential("yearly_sigma", lam=lam, shape=xf0.dshape[1])
+            ys = det_dot(xf0, yearly_sigma)
+            # a_sigma is in the interval (-b_sigma - ys, inf), mode at 0
             a_sigma = pm.Deterministic(
-                "a_sigma", tt.sub(pm.Exponential("as", lam=lam), b_sigma)
-            )
-            # sigma in (0, inf)
-            sigma = pm.Deterministic("sigma", a_sigma * gmtv + b_sigma)
+                "a_sigma", pm.Exponential("as", lam=lam) - b_sigma)
 
-            pm.Normal("obs", mu=mu, sigma=sigma, observed=df_valid["y_scaled"])
+            # sigma in (0, inf)
+            sigma = pm.Deterministic("sigma", a_sigma * gmtv + b_sigma + ys)
+
+            if not self.test:
+                pm.Normal("obs", mu=mu, sigma=sigma, observed=df_valid["y_scaled"])
 
         return model
 
