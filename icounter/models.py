4dcb4c676e741febc5ba9443f45306da9f43f260
Simon Treu <sitreu@pik-potsdam.de>
2020-02-10 12:30:58 +0100

    Normal distribution for Tasrange with sigma as lin function of mu

--- icounter/models.py
+++ icounter/models.py
@@ -1334,7 +1334,7 @@ class RsdsNormal(icounter.distributions.Normal):
         return model
 
 
-class Tasrange(icounter.distributions.Rice):
+class Tasrange(icounter.distributions.Normal):
 
     """ Influence of GMT is modelled through a shift of
     mu and sigma parameters in a Beta distribution.
@@ -1342,6 +1342,82 @@ class Tasrange(icounter.distributions.Rice):
 
     def __init__(self, modes):
         super(Tasrange, self).__init__()
+        self.modes = modes
+        self.test = False
+
+    def quantile_mapping(self, d, y_scaled):
+
+        """
+        specific for normally distributed variables.
+        """
+        quantile = stats.norm.cdf(y_scaled, loc=d["mu"], scale=d["sigma"])
+        x_mapped = stats.norm.ppf(quantile, loc=d["mu_ref"], scale=d["sigma_ref"])
+        x_mapped[x_mapped <= 0] = np.nan
+        # values are not alowed to become non-negative. If that would happen, do not quantile map
+        return x_mapped
+
+    def setup(self, df_subset):
+
+        model = pm.Model()
+
+        with model:
+            # FIXME: We can assume that all tas values are valid i think,
+            # so use df_subset directly.
+            df_valid = df_subset.dropna(axis=0, how="any")
+            gmtv = pm.Data("gmt", df_valid["gmt_scaled"].values)
+            xf0 = pm.Data("xf0", df_valid.filter(regex="^mode_0_").values)
+            xf1 = pm.Data("xf1", df_valid.filter(regex="^mode_1_").values)
+
+            # mu
+            # b_mu is in the interval (-inf,inf)
+            b_mu = pm.Normal("b_mu", mu=0.5, sigma=1)
+            # a_mu in (-inf, inf)
+            a_mu = pm.Normal("a_mu", mu=0, sigma=1)
+
+            fc_mu = pm.Normal("fc_mu", mu=0.0, sd=2.0, shape=xf0.dshape[1])
+            fctrend_mu = pm.Normal("fctrend_mu", mu=0.0, sd=2.0, shape=xf1.dshape[1])
+
+            # in (-inf, inf)
+            mu = pm.Deterministic(
+                "mu",
+                a_mu * gmtv
+                + b_mu
+                + det_dot(xf0, fc_mu)
+                + gmtv * det_dot(xf1, fctrend_mu),
+            )
+
+            # sigma
+            # b_sigma = pm.Lognormal("b_sigma", mu=-1, sigma=0.4, testval=1.0)
+            # a_sigma = pm.Normal("a_sigma", mu=0, sigma=0.05, testval=0)
+            #
+            # lin = pm.Deterministic(
+            #     "lin_sigma",
+            #     a_sigma * gmtv + b_sigma
+            # )
+            b_sigma = pm.Normal("b_sigma", mu=1
+                                , sigma=0.4, testval=1.0)
+            a_sigma = pm.Normal("a_sigma", mu=0, sigma=0.05, testval=0)
+
+            lin = pm.Deterministic(
+                "lin_sigma",
+                a_sigma * mu + b_sigma
+            )
+            alpha = 1e-6
+            sigma = pm.Deterministic("sigma", pm.math.switch(lin > alpha, lin, alpha))
+
+            if not self.test:
+                pm.Normal("obs", mu=mu, sigma=sigma, observed=df_valid["y_scaled"])
+        return model
+
+
+class TasrangeRice(icounter.distributions.Rice):
+
+    """ Influence of GMT is modelled through a shift of
+    mu and sigma parameters in a Beta distribution.
+    """
+
+    def __init__(self, modes):
+        super(TasrangeRice, self).__init__()
         self.modes = modes
         self.test = False
 
@@ -1517,10 +1593,10 @@ class Wind(icounter.distributions.Weibull):
 
             # beta
             b_beta = pm.Lognormal("b_beta", mu=-1, sigma=0.4, testval=1.0)
-            a_beta = pm.Normal("a_beta", mu=0, sigma=.1)
+            a_beta = pm.Normal("a_beta", mu=0, sigma=.01)
 
-            fc_beta = pm.Normal("fc_beta", mu=0.0, sd=.1, shape=xf0.dshape[1])
-            fctrend_beta = pm.Normal("fctrend_beta", mu=0.0, sd=.1, shape=xf1.dshape[1])
+            fc_beta = pm.Normal("fc_beta", mu=0.0, sd=.01, shape=xf0.dshape[1])
+            fctrend_beta = pm.Normal("fctrend_beta", mu=0.0, sd=.01, shape=xf1.dshape[1])
 
             lin_beta = pm.Deterministic(
                 "lin_beta",
@@ -1528,15 +1604,15 @@ class Wind(icounter.distributions.Weibull):
                 + b_beta
                 + det_dot(xf0, fc_beta)
             )
-            cutoff = 1e-6
+            cutoff = 1e-10
             beta = pm.Deterministic("beta", pm.math.switch(lin_beta > cutoff, lin_beta, cutoff))
 
             # beta
             # a alpha and b alpha could also be a function of GMT
             a_alpha = pm.Normal("a_alpha", mu=0.0, sd=.1)
-            b_alpha = pm.Normal("b_alpha", mu=0.0, sd=.1)
+            b_alpha = pm.Normal("b_alpha", mu=0, sd=5)
             lin_alpha = a_alpha * beta + b_alpha
-            cutoff = 1e-6
+            cutoff = 1e-10
             alpha = pm.Deterministic("alpha", pm.math.switch(lin_alpha > cutoff, lin_alpha, cutoff))
 
             if not self.test:
