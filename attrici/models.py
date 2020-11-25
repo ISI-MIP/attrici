c543877f2c58606bcc4a88648eb8a38ec844d1fe
Simon Treu <sitreu@pik-potsdam.de>
2020-11-25 08:38:16 +0100

    Add rsds-weibull model

--- attrici/models.py
+++ attrici/models.py
@@ -515,6 +515,64 @@ class Rsds(attrici.distributions.Normal):
         return model
 
 
+class RsdsWeibull(attrici.distributions.Weibull):
+    """ Influence of GMT is modelled through a shift of
+    the scale parameter beta in the Weibull distribution. The shape
+    parameter alpha is assumed free of a trend.
+
+    """
+
+    def __init__(self, modes):
+        super(RsdsWeibull, self).__init__()
+        self.modes = modes
+        self.test = False
+
+    def setup(self, df_subset):
+        model = pm.Model()
+
+        with model:
+            df_valid = df_subset.dropna(axis=0, how="any")
+            gmtv = pm.Data("gmt", df_valid["gmt_scaled"].values)
+            xf0 = pm.Data("xf0", df_valid.filter(regex="^mode_0_").values)
+
+            # beta
+            weights_longterm_intercept = pm.Normal("weights_longterm_intercept", mu=0, sd=1)
+            weights_longterm_trend = pm.Normal("weights_longterm_trend", mu=0, sd=0.1)
+            weights_fc_intercept = pm.math.concatenate(
+                [pm.Normal(f"weights_fc_intercept_{i}", mu=0, sd=1 / (2 * i + 1), shape=2)
+                 for i in range(int(xf0.dshape[1]) // 2)]
+            )
+            weights_fc_trend = pm.Normal("weights_fc_trend", mu=0, sd=0.1, shape=xf0.dshape[1])
+            weights_fc = pm.math.concatenate([
+                weights_fc_intercept,
+                weights_fc_trend
+            ])
+            # weights are the parameters that are learned by the model
+            # eta is a linear model of the predictors
+            eta = tt.dot(pm.math.concatenate([
+                xf0,
+                tt.tile(gmtv[:, None], (1, int(xf0.dshape[1]))) * xf0
+            ], axis=1),
+                weights_fc) + weights_longterm_intercept + weights_longterm_trend * gmtv
+            beta = pm.Deterministic("beta", pm.math.exp(eta))
+
+            # alpha
+            weights_alpha_longterm_intercept = pm.Normal("weights_alpha_longterm_intercept", mu=0, sd=1)
+            weights_alpha_fc_intercept = pm.math.concatenate(
+                [pm.Normal(f"weights_alpha_fc_intercept_{i}", mu=0, sd=1 / (2 * i + 1), shape=2)
+                 for i in range(int(xf0.dshape[1]) // 2)]
+            )
+
+            eta_alpha = tt.dot(xf0, weights_alpha_fc_intercept) + weights_alpha_longterm_intercept
+            alpha = pm.Deterministic("alpha", pm.math.exp(eta_alpha))
+            logp_ = pm.Deterministic("logp", model.logpt)
+
+            if not self.test:
+                pm.Weibull("obs", alpha=alpha, beta=beta, observed=df_valid["y_scaled"])
+
+        return model
+
+
 class Tasrange(attrici.distributions.Gamma):
     """ Influence of GMT is modelled through a shift of
     mu and sigma parameters in a Beta distribution.
