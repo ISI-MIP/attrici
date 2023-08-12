eef42ce5bc4ad7552f3da3b7260f4ef45585ffab
annabu <annabu@pik-potsdam.de>
2023-08-12 16:10:47 +0200

    fixed write-out as pickle for traces and timeseries files

--- sanity_check/merge_files.py
+++ sanity_check/merge_files.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # coding: utf-8
-# """Merge trace and timeseries files into single netcdf files"""
+# """Merge trace and timeseries files into pickle files"""
 
 import os, sys
 import glob
@@ -8,6 +8,7 @@ from pathlib import Path
 import argparse
 import re
 import numpy as np
+import pandas as pd
 import xarray as xr
 import pickle
 
@@ -28,8 +29,7 @@ def merge_files(in_dir, traces_or_ts):
     Mege trace or timeseries files into single netcdf
     in_dir (str): folder path to file which should be merged to nc
     """
-    if traces_or_ts=="timeseries":
-        ts_dict = {}
+    if traces_or_ts=="timeseries":  ## TODO merge ts files in similar way as trace files via xarray
         for file in in_dir.glob("**/*lon*"):
             lat = get_float_from_string(file.parent.name)
             lon = get_float_from_string(file.stem.split("lon")[-1])
@@ -37,65 +37,76 @@ def merge_files(in_dir, traces_or_ts):
             hdf = pd.HDFStore(file, mode="r")
             df = hdf.get(hdf.keys()[0])
             ts = {f'{lat}_{lon}': df.to_dict()}
-            ts_dict.update(ts)
       
-        # write netcdf to disk
-        tile = re.findall(r"\d{5}", str(in_dir))[0]
-        traces_or_timeseries = in_dir.parent.stem
-        var_folder = in_dir.parent.parent.name
-        filename = "merged_" + traces_or_timeseries + "_" + var_folder[var_folder.find(f't{tile}'):].rsplit("_",1)[0]
-  
-        filepath = in_dir.parent.parent / f"{filename}.pickle" 
-        with open(filepath, 'wb') as f:
-            pickle.dump(ts_dict, f, protocol=pickle.HIGHEST_PROTOCOL)
-        
+            # write single timeseries files to pickle
+            tile = re.findall(r"\d{5}", str(in_dir))[0]
+            traces_or_timeseries = in_dir.parent.stem
+            var_folder = in_dir.parent.parent.name
+            filename = "merged_" + traces_or_timeseries + "_" + var_folder[var_folder.find(f't{tile}'):].rsplit("_",1)[0]
+            filepath = in_dir.parent.parent / f"{filename}.pickle" 
+            try:
+                with open(filepath, 'ab+') as f:
+                    pickle.dump(ts, f, protocol=pickle.HIGHEST_PROTOCOL)
+            except:
+                with open(filepath, 'wb') as f:
+                    pickle.dump(ts, f, protocol=pickle.HIGHEST_PROTOCOL)
+
         print("Wrote merged file to:", filepath) 
 
+            ## reload pickle file via:
+            # reloaded_merged_ts = []
+            # with open(filepath, "rb") as f:
+                # #reloaded_merged_ts = pickle.load(f)
+                # while True:
+                    # try:
+                        # reloaded_merged_ts.append(pickle.load(f))
+                    # except EOFError:
+                        # break
+
     else:
-      # Write from single parameter files to netcdf
-      parameter_files = []
-  
-      for file in in_dir.glob("**/*lon*"):
-          lat = get_float_from_string(file.parent.name)
-          lon = get_float_from_string(file.stem.split("lon")[-1])
-          data_vars = []
-          with open(file, "rb") as trace:
-              free_params = pickle.load(trace)
-          for key in free_params.keys():
-              try:
-                  d = np.arange(len(free_params[key]))
-              except TypeError as e:
-                  if str(e) == "len() of unsized object":
-                      d = np.arange(1)
-                  else:
-                      raise e
-              data_vars.append(
-                  xr.DataArray(
-                      dims=["lat", "lon", "d"],  #TODO check dim order if same as for final_cfact.nc4
-                      data=free_params[key].reshape((1,1,-1)),
-                      coords={
-                          "lat": ("lat", [lat]),
-                          "lon": ("lon", [lon]),
-                          "d": ("d", d),
-                      },
-                      name=key
-                  )
-              )
-          parameter_files.append(xr.merge(data_vars))
-      
-      parameter_files = xr.merge(parameter_files)
-                
-      # write netcdf to disk
-      tile = re.findall(r"\d{5}", str(in_dir)) [0]
-      traces_or_timeseries = in_dir.parent.stem
-      var_folder = in_dir.parent.parent.name
-      filename = "merged_" + traces_or_timeseries + "_" + var_folder[var_folder.find(f't{tile}'):].rsplit("_",1)[0]
+        # Write single trace files to netcdf
+        parameter_files = []
 
-      filepath = in_dir.parent.parent / f"{filename}.nc" 
-      parameter_files.to_netcdf(filepath)
-      print("Wrote merged file to:", filepath) 
-
-
+        for file in in_dir.glob("**/*lon*"):
+            lat = get_float_from_string(file.parent.name)
+            lon = get_float_from_string(file.stem.split("lon")[-1])
+            data_vars = []
+            with open(file, "rb") as trace:
+                free_params = pickle.load(trace)
+            for key in free_params.keys():
+                try:
+                    d = np.arange(len(free_params[key]))
+                except TypeError as e:
+                    if str(e) == "len() of unsized object":
+                        d = np.arange(1)
+                    else:
+                        raise e
+                data_vars.append(
+                    xr.DataArray(
+                        dims=["lat", "lon", "d"],  #TODO check dim order if same as for final_cfact.nc4
+                        data=free_params[key].reshape((1,1,-1)),
+                        coords={
+                            "lat": ("lat", [lat]),
+                            "lon": ("lon", [lon]),
+                            "d": ("d", d),
+                        },
+                        name=key
+                    )
+                )
+ 
+            # write to disk
+            tile = re.findall(r"\d{5}", str(in_dir)) [0]
+            traces_or_timeseries = in_dir.parent.stem
+            var_folder = in_dir.parent.parent.name
+            filename = "merged_" + traces_or_timeseries + "_" + var_folder[var_folder.find(f't{tile}'):].rsplit("_",1)[0]
+            filepath = in_dir.parent.parent / f"{filename}.pickle" 
+            try:
+                with open(filepath, 'ab+') as f:
+                    pickle.dump(xr.merge(data_vars), f, protocol=pickle.HIGHEST_PROTOCOL)
+            except:
+                with open(filepath, 'wb') as f:
+                    pickle.dump(xr.merge(data_vars), f, protocol=pickle.HIGHEST_PROTOCOL)
+        print("Wrote merged file to:", filepath) 
 
 
 
