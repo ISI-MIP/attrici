610474ab706c6fca4310ca2c4038a7e76bfb1a50
AnnaBooki <a.buch@stud.uni-heidelberg.de>
2023-08-16 10:40:34 +0200

    fixed: one task runs one variable by adding --exclusive

--- mpi_slurm.sh
+++ mpi_slurm.sh
@@ -1,36 +1,41 @@
 #!/bin/bash
 
-#SBATCH --qos=short #standby   #short
-#SBATCH --partition=standard #priority
-#SBATCH --job-name=attrici_mpi_run_estimation
+#SBATCH --qos=short #priority standby 
+#SBATCH --partition=standard ## standard #priority
+#SBATCH --job-name=attrici_mpi_t2_run_estimation
 #SBATCH --account=dmcci 
-#SBATCH --nodes=1   # 4 with 1 task-per-node= one var processed by 4 nodes at same time
-#SBATCH --ntasks-per-node=2  # 1 = only one var is process - probably as threating process
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=annabu@pik-potsdam.de
+#SBATCH --nodes=1    
+#SBATCH --ntasks-per-node=3
 #SBATCH --array=0-100
-#SBATCH --cpus-per-task=8
-#SBATCH --output=/p/tmp/annabu/projects/attrici/log/mpi/%x/run_estimation_%A_%a_mpi-%j.log
-#SBATCH --output=/p/tmp/annabu/projects/attrici/log/mpi/%x/run_estimation_%A_%a_mpi-%j.log
-#SBATCH --time=00-06:00:00
+#SBATCH --cpus-per-task=10  ## same as OMP_NUM_THEADS
+#SBATCH --output=/p/tmp/annabu/projects/attrici/log/00002/run_estimation_%A_%a_mpi-%j.out
+#SBATCH --error=/p/tmp/annabu/projects/attrici/log/00002/run_estimation_%A_%a_mpi-%j.out
+#SBATCH --time=07:30:00  
+
+## SLURM_NTASKS = nodes * ntaskes-per-node
 
 export CXX=g++
 tmpdir=/p/tmp/annabu/projects/attrici/tmp/theano_${SLURM_JOB_ID}_${SLURM_ARRAY_TASK_ID}_${SLURM_NTASKS}.tmp
 mkdir -p $tmpdir
 export TMPDIR=$tmpdir
 
+
+
 ## test if uncomment or modification needed
 ## if you're using OpenMP for threading
-# unset I_MPI_DAPL_UD
-# unset I_MPI_DAPL_UD_PROVIDER
+#unset I_MPI_DAPL_UD   # TEST: as uncommented
+#unset I_MPI_DAPL_UD_PROVIDER
 
-## srun version
+## get mpi
 export I_MPI_PMI_LIBRARY=/p/system/slurm/lib/libpmi.so
 
-## test if uncomment or modification needed
 # if you're using OpenMP for threading
 export OMP_PROC_BIND=true # make sure our threads stick to cores
-export OMP_NUM_THREADS=2  # matches how many cpus-per-task we asked for  # todo test change value or uncomment
+export OMP_NUM_THREADS=10   # matches how many cpus-per-task we asked for  # todo test change value or uncomment
+# OMP_NUM_THREADS: so that OpenMP knows how many threads to use per task.
 export SUBMITTED=1
-
 compiledir=/p/tmp/annabu/projects/attrici/.pytensor/${SLURM_JOB_ID}_${SLURM_ARRAY_TASK_ID}
 mkdir -p $compiledir
 export PYTENSOR_FLAGS=base_compiledir=$compiledir
@@ -40,8 +45,6 @@ echo "------------------------------------------------------------"
 echo "SLURM JOB ID: $SLURM_JOBID"
 echo "$SLURM_NTASKS tasks"
 echo "------------------------------------------------------------"
-# we can use "-n 64" (4*16) in the run commands below or the 
-# shortcut $SLURM_NTASKS
 # See http://slurm.schedmd.com/sbatch.html for a full list of 
 # input and output variables
 
@@ -51,31 +54,18 @@ cleanup() {
   rm -r ${tmpdir}
   exit
 }
+
 cp .pytensorrc /home/annabu/.pytensorrc
 trap cleanup SIGTERM SIGINT
 
-for var_folder in attrici_03_era5_t00002_sfcWind_rechunked attrici_03_era5_t00002_tas18_rechunked attrici_03_era5_t00002_rsds_rechunked # ; do
+#for var_folder in attrici_03_era5_t00002_sfcWind_rechunked attrici_03_era5_t00002_tas18_rechunked  # works fine
+for var_folder in attrici_03_era5_t00002_pr0_rechunked attrici_03_era5_t00002_tasskew_rechunked attrici_03_era5_t00002_hurs_rechunked;
 do
     echo ${var_folder}
-    srun -n $SLURM_NTASKS /home/annabu/.conda/envs/attrici_pymc5_2/bin/python -u /p/tmp/annabu/projects/attrici/runscripts/attrici_automated_processing/00002/${var_folder}/run_estimation.py  #todo test with & 
+    srun --exclusive --ntasks 1  /home/annabu/.conda/envs/attrici_pymc5_2/bin/python -u /p/tmp/annabu/projects/attrici/runscripts/attrici_automated_processing/00002/${var_folder}/run_estimation.py  & # --exclusive --ntasks 1 : without srun would use full allocation for each variable
+    
+    #    srun -n $SLURM_NTASKS  /home/annabu/.conda/envs/attrici_pymc5_2/bin/python -u /p/tmp/annabu/projects/attrici/runscripts/attrici_automated_processing/00002/${var_folder}/run_estimation.py  #todo test with &   # SLURM_NTASKS=ntask-per-node
 done
 wait
 
-
 cleanup
-
-## EXAMPLES 
-#srun -n $SLURM_NTASKS ./sumprimes 0 1000000
-# same py script for  multiple infiles
-#for i in {1..100}  
-#do
-#    srun -n 1 python3 xxx.py -i /path/input$i &
-#done
-#wait
-
-
-# alternatively, use mpirun directly. We have to have the modules
-# loaded by this point.
-# e.g. "module load intel/2017.1"
-#mpirun -bootstrap slurm -n $SLURM_NTASKS \ 
-#  $HOME/cluster-examples/sumprimes/mpi/sumprimes 0 10000000
