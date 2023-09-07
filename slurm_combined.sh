d6932ffe857a5e919e5faf8e9dd9e855451803be
annabu <annabu@pik-potsdam.de>
2023-09-07 16:03:00 +0200

    fixed user arguments, uncomment mail address

---
+++ slurm_combined.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#### entire workflow for one variable, overwrites jobname and log files in sanity_checks.sh and merge_files.sh ####
+
+tile=$1
+var=$2
+outfile_sanity=/p/tmp/annabu/projects/attrici/log/attrici_03_era5_t${tile}_${var}_rechunked/sanity_check_${tile}_${var}.log
+outfile_merge=/p/tmp/annabu/projects/attrici/log/attrici_03_era5_t${tile}_${var}_rechunked/merge_files_${tile}_${var}.log
+
+
+# create trace and ts files
+cd /p/tmp/annabu/projects/attrici/runscripts/attrici_automated_processing/${tile}/attrici_03_era5_t${tile}_${var}_rechunked/
+jobid=$(sbatch --parsable slurm.sh)
+
+# sanity check
+cd /p/tmp/annabu/projects/attrici/
+jobid_sanity=$(sbatch --parsable --dependency=afterany:$jobid --job-name=attrici_sanity_checks_${tile}_${var} --output=${outfile_sanity} --error=${outfile_sanity} sanity_checks.sh ${tile} ${var})
+
+# merge traces
+sbatch --dependency=afterok:$jobid_sanity --job-name=attrici_merge_files_${tile}_${var} --output=${outfile_merge} --error=${outfile_merge} merge_files.sh ${tile} ${var}
+
+# create final cfacts
+cd /p/tmp/annabu/projects/attrici/runscripts/attrici_automated_processing/${tile}/attrici_03_era5_t${tile}_${var}_rechunked/
+sbatch --dependency=afterok:$jobid_sanity submit_write_netcdf.sh   #  create netcdf
+
--- submit_write_netcdf.sh
+++ submit_write_netcdf.sh
@@ -4,15 +4,16 @@
 # name of your run at all locations. Replace also --mail-user
 
 #SBATCH --job-name=runid_merge
-#SBATCH --account=isimip
+#SBATCH --account=dmcci
 #SBATCH --output=/p/tmp/annabu/projects/attrici/log/%x/%A_%a.log
 #SBATCH --error=/p/tmp/annabu/projects/attrici/log/%x/%A_%a.log
-##SBATCH --mail-type=END,FAIL
-##SBATCH --mail-user=annabu@pik-potsdam.de
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=annabu@pik-potsdam.de
 
 # block one node to have enough memory
 #SBATCH --partition=largemem
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
+#SBATCH --time=23:59:00
 
 /home/annabu/.conda/envs/attrici_pymc5_2/bin/python -u write_netcdf.py
