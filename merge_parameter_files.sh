066b3a08c5fc7e7984557b10e00ecd9b9a2eb518
Simon Treu <sitreu@pik-potsdam.de>
2024-10-02 09:56:22 +0200

    Update merge_parameter_files to use a singularity container

--- merge_parameter_files.sh
+++ merge_parameter_files.sh
@@ -8,6 +8,22 @@
 #SBATCH --cpus-per-task=6
 #SBATCH --time=23:30:00
 
+module purge
+module load singularity
+
 source ./variables_for_shellscripts.sh
 
-$attrici_python -u ./merge_parameter_files.py
+tmpdir=$project_basedir/singularity_tmp/${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}
+mkdir -p $tmpdir
+export SINGULARITY_TMPDIR=$tmpdir
+
+cleanup() {
+  rm -rf ${tmpdir}
+  exit
+}
+
+trap cleanup SIGTERM SIGINT
+
+singularity exec -B /p:/p ${project_basedir}/ATTRICI.sif bash -c "cd $RUNDIR; python -u merge_parameter_files.py"
+cleanup
+echo "finished merging the parameter files"
