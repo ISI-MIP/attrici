73ec57c3fc4d85946dbdea234c6f0f27bb8cfcf9
Robert Gieseke <rob.g@web.de>
2024-11-28 15:40:07 +0100

    Include code for Singular Spectrum Analysis (SSA) directly (#102)
    * Add test for SSA processimg
    
    * Vendor Singular Spectrum Analysis from pyts
    
    Taken from
    https://github.com/johannfaouzi/pyts/blob/0f023a6afc1b1910bd59aedf359919e378c43fe2/pyts/decomposition/ssa.py
    
    * Clean-up vendored version and remove numba dependency
    
    Include tests for SSA from upstream.
    Note that the vendored version works slightly differently from version
    0.13 of pyts which introduced chunking and parallelisation so the
    variable output in `preproccesin.py` was adjusted.
    
    Co-authored-by: Sven Willner <sven.willner@akator.de>
    Co-authored-by: Robert Gieseke <rob.g@web.de>
    
    * Suppress warning
    
    Warning was:
    "RuntimeWarning: numpy.ndarray size changed, may indicate binary
    incompatibility. Expected 16 from C header, got 96 from PyObject"
    
    * Adjust test accuracy
    
    ---------
    
    Co-authored-by: Sven Willner <sven.willner@akator.de>

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,4 +1,11 @@
+import subprocess
+
+# TODO check whether still needed once updated to latest numpy version
+import netCDF4  # noqa Used to suppress warning https://github.com/pydata/xarray/issues/7259
 import numpy as np
+import pytest
+import xarray as xr
+from loguru import logger
 
 from attrici import preprocessing
 
@@ -18,3 +25,34 @@ def test_calc_gmt_by_ssa():
 
     assert ssa.shape == (len(times[::subset]),)
     assert ssa_times.shape == (len(times[::subset]),)
+
+
+@pytest.mark.slow
+def test_ssa():
+    INPUT_FILE = "tests/data/20crv3-era5_gmt_raw.nc"
+    OUTPUT_FILE = "tests/data/output/20crv3-era5_gmt_ssa.nc"
+    REFERENCE_FILE = "tests/data/20CRv3-ERA5_germany_ssa_gmt.nc"
+    MAX_DIFFERENCE = 1e-3
+
+    command = [
+        "attrici",
+        "ssa",
+        "--variable",
+        "tas",
+        "--window-size",
+        "365",
+        "--subset",
+        "10",
+        INPUT_FILE,
+        OUTPUT_FILE,
+    ]
+
+    logger.info("Running command: {}", " ".join(command))
+
+    status = subprocess.run(command, check=False)
+    status.check_returncode()
+
+    reference_data = xr.load_dataset(REFERENCE_FILE)
+    output = xr.load_dataset(OUTPUT_FILE)
+
+    assert abs(reference_data.tas - output.ssa).max() < MAX_DIFFERENCE
